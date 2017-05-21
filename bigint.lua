1097f78d65dd95266090460ed63b061576f5421f
Stas Mikhel <mikhel.sk@gmail.com>
2017-05-21 14:40:18 +0300

    Fix bug with div function

--- bigint.lua
+++ bigint.lua
@@ -1,26 +1,35 @@
+-- Operations with arbitrary integer numbers
 
 local bigint = {}
 bigint.__index = bigint
 
-bigint.BASE = 10
-bigint.type = 'bigint'
+-- parameters
+bigint.BASE = 10        -- the radix
+bigint.type = 'bigint'  -- mark of type
 
+-- description
+bigint.about = {}
+
+-- absolute value of integer
 local function iabs(v) return (v < 0) and (-v) or v end
 
+-- convert integer into bigint
 local function fromint(v) 
    v = math.tointeger(v)
    assert(v, 'Integer is expected')
-   return tostring(iabs(v)), (v < 0) and -1 or 1   
+   return tostring(iabs(v)), (v < 0) and -1 or 1   -- return value and sing
 end
 
+-- convert string into bigint
 local function fromstr(s)
-   assert(string.find(s, '^[+-]?%d+$'), "Wrong string number")
-   return string.match(s, '%d+'), (string.byte(s) == string.byte('-')) and -1 or 1
+   assert(string.find(s, '^[+-]?%d+$'), "Wrong string number")                       -- check format
+   return string.match(s, '%d+'), (string.byte(s) == string.byte('-')) and -1 or 1   -- return value and sing
 end
 
+-- constructor
 function bigint:new(a)
    local s, sign
-   if type(a) == 'number' then 
+   if     type(a) == 'number' then 
       s, sign = fromint(a)
    elseif type(a) == 'string' then
       s, sign = fromstr(a)
@@ -31,7 +40,12 @@ function bigint:new(a)
    setmetatable(o, self)
    return o
 end
+bigint.about[bigint.new] = [[
+  : bigint:new(val)
+Create an arbitrary long integer from integer or string.
+]]
 
+-- reduce front zeros
 local function simplify (v)
    local i = #v
    while i > 1 and v[i] == 0 do
@@ -40,13 +54,8 @@ local function simplify (v)
    end
 end
 
-local function args (a, b)
-   --[[
-   a = (type(a) == 'number' or type(a) == 'string') and bigint:new(a) or a
-   if b then
-      b = (type(b) == 'number' or type(b) == 'string') and bigint:new(b) or b
-   end
-   ]]
+-- correct function arguments if need
+local function args (a, b)   
    a = (type(a) == 'table' and a.type == bigint.type) and a or bigint:new(a)
    if b then
       b = (type(b) == 'table' and b.type == bigint.type) and b or bigint:new(b)
@@ -54,91 +63,130 @@ local function args (a, b)
    return a, b
 end
 
-
+-- main function for division
 local function div(a,b)
    a,b = args(a,b)
-   local num = string.reverse(a.value)
-   local acc = {}
-   local k = #b.value
-   local rest = bigint:new(string.sub(num, 1, k))
-   local denom = bigint.abs(b)
-   while k <= #num do
-      if rest >= denom then
-         local n = bigint.BASE
-	 local prod
-	 repeat
+   local num = string.reverse(a.value)            -- numerator as string
+   local acc = {}                                 -- result
+   local k = #b.value                             -- index of the last character
+   local rest = bigint:new(string.sub(num, 1, k)) -- current part of numerator
+   local denom = bigint.abs(b)                    -- denominator
+   -- read the string
+   while k <= #num do                             
+      if rest >= denom then                       -- try to devide
+         local n, prod = bigint.BASE, nil
+	 repeat                                   -- broot force search for multiplier (
 	    n = n-1
 	    prod = denom*n
 	 until prod <= rest
+	 -- save result
 	 acc[#acc+1] = n
 	 rest = rest-prod
       else
          if #acc > 0 then acc[#acc+1] = 0 end
       end
       k = k+1
+      -- update current numerator
       rest.value = string.sub(num, k,k) .. rest.value
    end
+   -- convert result
    local result = bigint:new(a.sign*b.sign)
-   result.value = #acc > 0 and string.reverse(table.concat(acc)) or '0'
+   result.value = (#acc > 0) and string.reverse(table.concat(acc)) or '0'
    return result, rest   
 end
 
+local function sum(a,b)
+   local acc = {}
+   local zero = string.byte('0')
+   for i = 1, math.max(#a.value,#b.value) do
+      local ai = string.byte(a.value, i) or zero
+      local bi = string.byte(b.value, i) or zero      
+      acc[i] = (acc[i] or 0) + (ai-zero) + (bi-zero)
+      if acc[i] >= bigint.BASE then
+         acc[i] = acc[i] - bigint.BASE
+         acc[i+1] = 1
+      end      
+   end
+   simplify(acc)
+   local res = bigint:new(0)
+   res.value = table.concat(acc)
+   return res
+end
+
+local function sub(a,b)
+   local p,q,r = a,b,1
+   if bigint.abs(a) < bigint.abs(b) then
+      p,q,r = q,p,-1
+   end
+   local acc = {}
+   local zero = string.byte('0')
+   for i = 1, #p.value do
+      local pi = string.byte(p.value, i) or zero
+      local qi = string.byte(q.value, i) or zero
+      acc[i] = (acc[i] or 0) + pi - qi    -- (pi-zero)-(qi-zero)
+      if acc[i] < 0 then
+         acc[i] = acc[i] + bigint.BASE
+         acc[i+1] = -1
+      end
+   end
+   simplify(acc)
+   local res = bigint:new(r)
+   res.value = table.concat(acc)
+   return res
+end
+
+-- module of number
 bigint.abs = function (v)
    local a = (type(v) == 'number' or type(v) == 'string') and bigint:new(v) or bigint.copy(v)
    if a.sign < 0 then a.sign = -a.sign end
    return a
 end
+bigint.about[bigint.abs] = [[
+  : bigint.abs(v)
+Return module of arbitrary long number.
+]]
 
+-- get copy
 bigint.copy = function (v)
    local c = bigint:new(v.sign)
    c.value = v.value
    return c
 end
+bigint.about[bigint.copy] = [[
+  : bigint.copy(v)
+Return copy of given number.
+]]
 
+-- a + b
 bigint.__add = function (a,b)
-   a,b = args(a,b)
-   local zero = string.byte('0')
-   local sum = {}
-   local ci, d = 0, 0
-   for i = 1, math.max(#a.value, #b.value) do
-      local ai = string.byte(a.value, i) or zero
-      local bi = string.byte(b.value, i) or zero
-      ci = a.sign * (ai - zero) + b.sign * (bi - zero) + d
-      if ci >= bigint.BASE then 
-         sum[i] = ci - bigint.BASE
-         d = 1
-      elseif ci <= -bigint.BASE then
-         sum[i] = -ci - bigint.BASE
-	 d = -1
-      else
-         sum[i] = iabs(ci)
-	 d = 0
-      end
-   end
-   if d ~= 0 then sum[#sum+1] = 1 end
-   simplify(sum)
-   -- save
-   local res = bigint:new(0)
-   res.sign = (ci < 0) and -1 or 1
-   res.value = table.concat(sum)
-   return res
+   a,b = args(a,b) 
+   local res
+   if     a.sign > 0 and b.sign > 0 then return sum(a,b)
+   elseif a.sign < 0 and b.sign < 0 then return -sum(a,b)
+   elseif a.sign > 0 and b.sign < 0 then return sub(a,b)
+   else                                  return sub(b,a)
+   end   
 end
 
+-- -v
 bigint.__unm = function (v)
    local res = bigint:new(-v.sign)
    res.value = v.value
    return res
 end
 
+-- a - b
 bigint.__sub = function (a, b)
    b = args(b)
    return bigint.__add(a, -b)
 end
 
+-- a * b
 bigint.__mul = function (a, b) 
    a,b = args(a,b)
    local zero = string.byte('0')
-   local sum = {}   
+   local sum = {}
+   -- get products   
    for i = 1, #a.value do
       local ai = string.byte(a.value, i) - zero
       for j = 1, #b.value do
@@ -151,7 +199,7 @@ bigint.__mul = function (a, b)
    local d = 0
    for i = 1, #sum do
       sum[i] = sum[i] + d
-      d = math.floor(sum[i]/bigint.BASE)
+      d = math.floor(sum[i] / bigint.BASE)
       sum[i] = math.floor(sum[i] % bigint.BASE)
    end
    if d ~= 0 then sum[#sum+1] = d end
@@ -162,41 +210,48 @@ bigint.__mul = function (a, b)
    return res   
 end
 
+-- a / b
 bigint.__div = function (a, b)
    local res, _ = div(a,b)
    return res
 end
 
+-- a % b
 bigint.__mod = function (a, b)
    local _, res = div(a,b)
    return res
 end
 
+-- a == b
 bigint.__eq = function (a,b)
    a,b = args(a,b)
    return a.sign == b.sign and a.value == b.value
 end
 
+-- a < b
 bigint.__lt = function (a,b)
    a,b = args(a,b)
    if a.sign < b.sign then return true end
-   if #a.value == #b.value then
+   if #a.value == #b.value then         -- equial length
       local va, vb = string.reverse(a.value), string.reverse(b.value)
       return (a.sign > 0 and va < vb) or (a.sign < 0 and va > vb)
-   else
+   else                                 -- different length
       return (a.sign > 0 and #a.value < #b.value) or (a.sign < 0 and #a.value > #b.value) 
    end
 end
 
+-- a <= b
 bigint.__le = function (a,b)
    a,b = args(a,b)
    return bigint.__eq(a,b) or bigint.__lt(a,b)
 end
 
+-- #v
 bigint.__len = function (v)
    return #v.value
 end
 
+-- a^b
 bigint.__pow = function (a,b)
    a,b = args(a,b)
    assert(b.sign >= 0, "Power must be non negative")
@@ -204,32 +259,52 @@ bigint.__pow = function (a,b)
       assert(a ~= 0, "Error: 0^0")
       return bigint:new(1) 
    end
-   local aa, bb = bigint.copy(a), bigint.copy(b)
+   local aa, bb, q = bigint.copy(a), bigint.copy(b), nil
    local res = bigint:new(1)
    local h = 1
    while bb > 0 do
-      local p,q = div(bb,2)
+      bb,q = div(bb,2)
       if q.value ~= '0' then
          res = res * aa
       end
-      aa = aa * aa
-      bb = p
+      aa = aa * aa      
    end
    return res
 end
 
+-- string representation
 bigint.__tostring = function (v)
    return (v.sign < 0 and '-' or '') .. string.reverse(v.value)
 end
 
+-- float number representation
 bigint.tonumber = function (v)
    return tonumber(bigint.__tostring(v))
 end
 
+bigint.factorial = function (m)
+   local n = bigint.abs(m)
+   local res = bigint:new(1)   
+   local k = 0
+   while n > 0 do   
+      print(n)   
+      res = res * n
+      n = n - 1      
+      k = k + 1
+      if k == 20 then break end
+   end
+   return res
+end
+
 a = bigint:new(25)
-b = bigint:new(2)
+b = bigint:new(10)
+c = bigint:new(1)
 
 print(a ^ 36 )
+
+print(bigint.factorial(11))
+--print(bigint:new(10) > 0)
+--print(-a-b)
 
 
 
