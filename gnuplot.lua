04e0c61225cab11c90a3e10087a2ffa9d3fa52e0
Stas Mikhel <mikhel.sk@gmail.com>
2017-06-07 08:13:29 +0300

    Change plot function

--- gnuplot.lua
+++ gnuplot.lua
@@ -23,28 +23,28 @@ gnuplot.set_fn = function (g, f, l) table.insert(g, {f,legend=l}) end
 
 gnuplot.set_xrange = function (g, x1, x2, dx) 
    g.xrange = {x1, x2, dx} 
-   gnuplot.cmd(g, string.format("set xrange [%d:%d]", x1, x2))
+   --gnuplot.cmd(g, string.format("set xrange [%d:%d]", x1, x2))
 end
 
 gnuplot.set_yrange = function (g, y1, y2, dy) 
    g.yrange = {y1, y2, dy} 
-   gnuplot.cmd(g, string.format("set yrange [%d:%d]", y1, y2))
+   --gnuplot.cmd(g, string.format("set yrange [%d:%d]", y1, y2))
 end
 
 
 gnuplot.set_xlabel = function (g, lbl) 
    g.xlabel = lbl 
-   gnuplot.cmd(g, string.format('set xlabel "%s"', lbl))
+   --gnuplot.cmd(g, string.format('set xlabel "%s"', lbl))
 end
 
 gnuplot.set_ylabel = function (g, lbl) 
-   g.ylable = lbl 
-   gnuplot.cmd(g, string.format('set ylabel "%s"', lbl))
+   g.ylabel = lbl 
+   --gnuplot.cmd(g, string.format('set ylabel "%s"', lbl))
 end
 
 gnuplot.set_title = function (g, txt) 
    g.title = txt 
-   gnuplot.cmd(g, string.format('set title "%s"', txt))
+   --gnuplot.cmd(g, string.format('set title "%s"', txt))
 end
 
 gnuplot.set_permanent = function (g, b) g.permanent = (b or true) end
@@ -59,11 +59,12 @@ gnuplot.plot = function (g)
    if g.handle then g.handle:close() end
    g.handle = io.popen('gnuplot' .. (g.permanent and ' -p' or ''), 'w')
    -- set range
-   if g.xrange then gnuplot.set_xrange(g, table.unpack(g.xrange)) end
-   if g.yrange then gnuplot.set_yrange(g, table.unpack(g.yrange)) end
-   if g.xlabel then gnuplot.set_xlabel(g, g.xlabel) end
-   if g.ylabel then gnuplot.set_ylabel(g. g.ylabel) end
-   if g.title then gnuplot.set_title(g, g.title) end
+   local cmd = {}
+   if g.xrange then cmd[#cmd+1] = string.format('set xrange [%d:%d]', g.xrange[1], g.xrange[2]) end
+   if g.yrange then cmd[#cmd+1] = string.format('set yrange [%d:%d]', g.yrange[1], g.yrange[2]) end
+   if g.xlabel then cmd[#cmd+1] = string.format('set xlabel "%s"', g.xlabel) end
+   if g.ylabel then cmd[#cmd+1] = string.format('set ylabel "%s"', g.ylabel) end
+   if g.title then cmd[#cmd+1] = string.format('set title "%s"', g.title) end
 
    -- prepare command set
    local plot = {}
@@ -76,7 +77,8 @@ gnuplot.plot = function (g)
       plot[#plot+1] = str
    end
    local plot_fn = 'plot ' .. table.concat(plot, ',')
-   gnuplot.cmd(g, plot_fn)
+   cmd[#cmd+1] = plot_fn
+   gnuplot.cmd(g, table.concat(cmd,'\n'))
    -- settings
    
    return getmetatable(g) and g or gnuplot:new(g)
