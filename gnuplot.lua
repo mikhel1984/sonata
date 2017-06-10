2f9d9372ec4f2c8647fe2a60bae45fdb661b4076
Stas Mikhel <mikhel.sk@gmail.com>
2017-06-10 07:55:11 +0300

    Internal functions and smoothing for gnuplot

--- gnuplot.lua
+++ gnuplot.lua
@@ -31,37 +31,21 @@ gnuplot.cmd = function (g, cmd)
    end
 end
 
-gnuplot.set_fn = function (g, f, l) 
-   table.insert(g, {f, legend=l, type=FUNCTION}) 
-end
+gnuplot.set_fn = function (g, f, l) g[#g+1] = {f, legend=l, type=FUNCTION} end
 
-gnuplot.set_src = function (g, s, l)
-   table.insert(g, {s, legend=l, type=DATA})
-end
+gnuplot.set_src = function (g, s, l) g[#g+1] = {s, legend=l, type=DATA} end
 
-gnuplot.set_xrange = function (g, x1, x2, dx) 
-   g.xrange = {x1, x2, dx} 
-end
+gnuplot.set_xrange = function (g, x1, x2, dx) g.xrange = {x1, x2, dx} end
 
-gnuplot.set_yrange = function (g, y1, y2, dy) 
-   g.yrange = {y1, y2, dy} 
-end
+gnuplot.set_yrange = function (g, y1, y2, dy) g.yrange = {y1, y2, dy} end
 
+gnuplot.set_xlabel = function (g, lbl) g.xlabel = lbl end
 
-gnuplot.set_xlabel = function (g, lbl) 
-   g.xlabel = lbl 
-   --gnuplot.cmd(g, string.format('set xlabel "%s"', lbl))
-end
+gnuplot.set_ylabel = function (g, lbl) g.ylabel = lbl end
 
-gnuplot.set_ylabel = function (g, lbl) 
-   g.ylabel = lbl 
-   --gnuplot.cmd(g, string.format('set ylabel "%s"', lbl))
-end
+gnuplot.set_title = function (g, txt) g.title = txt end
 
-gnuplot.set_title = function (g, txt) 
-   g.title = txt 
-   --gnuplot.cmd(g, string.format('set title "%s"', txt))
-end
+gnuplot.set_smooth = function (g, sm) g.smooth = sm end
 
 gnuplot.set_separator = function (g, sep) g.separator = sep end
 
@@ -71,13 +55,32 @@ gnuplot.close = function (g)
    if g.handle then g.handle:close() end 
 end
 
+local function tmptable(t)
+   local name = os.tmpname()
+   local f = io.open(name, 'w')
+   for _, row in ipairs(t) do
+      for i, val in ipairs(row) do f:write((i>1 and ',' or ''), val) end
+      f:write('\n'); f:flush()
+   end
+   f:close()
+   return name
+end
+
+local function tmpfunction(fn, from, to, step)
+   local name = os.tmpname()
+   local f = io.open(name, 'w')
+   for x = from, to, step do f:write(x, ',', fn(x), '\n') end
+   f:flush(); f:close()
+   return name
+end
 
 gnuplot.plot = function (g)
    -- reopen window
    if g.handle then g.handle:close() end
    g.handle = io.popen('gnuplot' .. (g.permanent and ' -p' or ''), 'w')
-   -- set range
+   -- settings
    local cmd = {}
+   g.xrange = g.xrange or {-10,10}
    if g.xrange then cmd[#cmd+1] = string.format('set xrange [%d:%d]', g.xrange[1], g.xrange[2]) end
    if g.yrange then cmd[#cmd+1] = string.format('set yrange [%d:%d]', g.yrange[1], g.yrange[2]) end
    if g.xlabel then cmd[#cmd+1] = string.format('set xlabel "%s"', g.xlabel) end
@@ -89,21 +92,24 @@ gnuplot.plot = function (g)
    local plot = {}
    local file_list = {}
    for _,f in ipairs(g) do
-      local str
+      local str = ''
       if not f.type or f.type == FUNCTION then
-         if type(f[1]) == 'string' then 
-            str = f[1]
-         end
-      elseif f.type == DATA then
-         local fname = ''
-         if type(f[1]) == 'table' then
-	    fname = os.tmpname()
-	    tocsv(fname, f[1])
-	    table.insert(file_list, fname)
-	 elseif type(f[1]) == 'string' then
-	    fname = f[1]
+         if type(f[1]) == 'string' then str = f[1]
+	 elseif type(f[1]) == 'function' then
+	    g.xrange = g.xrange or {-10,10}
+	    g.xrange[3] = g.xrange[3] or (g.xrange[2]-g.xrange[1])/100
+	    str = tmpfunction(f[1], table.unpack(g.xrange))
+	    table.insert(file_list, str)
+	    str = '\"'..str..'\" smooth unique'
 	 end
-	 str = '\"'..fname..'\"'
+      elseif f.type == DATA then
+         if type(f[1]) == 'table' then
+	    str = tmptable(f[1])
+	    table.insert(file_list, str)
+	 elseif type(f[1]) == 'string' then
+	    str = f[1]
+	 end
+	 str = '\"'..str..'\" smooth unique'
       end
       if f.legend then str = string.format('%s title "%s"', str, f.legend) end
       plot[#plot+1] = str
@@ -133,8 +139,7 @@ g:set_title("Test")
 g:plot()
 ]]
 
-a = {{1,2},{2,2},{3,3},{4,4},{5,5}}
 g = gnuplot:new()
 g:set_permanent()
-g:set_src(a, "table")
+g:set_fn(math.sin, "sinus")
 g:plot()
