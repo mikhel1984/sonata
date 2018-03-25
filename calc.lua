f953882ee8f79f0ac4abbb4fa4cd8744b0ae8c2c
Stas Mikhel <mikhel.sk@gmail.com>
2018-03-25 22:32:01 +0300

    ...

--- calc.lua
+++ calc.lua
@@ -19,7 +19,7 @@ LC_USE_COLOR = true
 --====================  CODE  ========================
 
 -- Version
-lc_version = '0.8.1'
+lc_version = '0.8.2'
 
 -- Add path to the libraries
 if LC_ADD_PATH then
