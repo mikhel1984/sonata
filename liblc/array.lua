45d0cb58b59ea3d739efa3c1354895d3afa8fba3
Stas <mikhel.sk@gmail.com>
2018-06-18 19:39:54 +0300

    Rename variables

--- liblc/array.lua
+++ liblc/array.lua
@@ -68,9 +68,9 @@ local Ver = require "liblc.versions"
 local function isarray(t) return type(t) == 'table' and t.isarray end
 
 -- Prepare list of coefficients
-local function getk (s)
+local function getk (tsize)
    local k = {1}
-   for i = 2, #s do k[i] = s[i-1]*k[i-1] end
+   for i = 2, #tsize do k[i] = tsize[i-1]*k[i-1] end
    return k
 end
 
@@ -80,6 +80,7 @@ local help = lc_version and (require "liblc.help") or {new=function () return {}
 -- 	MODULE
 
 local array = {
+-- mark
 type='array', isarray=true,
 -- elementary function definition
 _add_ = function (x,y) return x+y end,
@@ -95,24 +96,24 @@ array.__index = array
 array.about = help:new("Manipulations with arrays of elements.")
 
 -- Create new object, set metatable.
-array.new = function (self, s)
-   return setmetatable({size=s, k=getk(s)}, self)
+array.new = function (self, tsize)
+   return setmetatable({size=tsize, k=getk(tsize)}, self)
 end
 
 -- Check index correctness.
-array._icorrect = function (arr, ind)
-   if #arr.size ~= #ind then return false end
-   for i = 1,#ind do 
-      if ind[i] > arr.size[i] or ind[i] < 1 then return false end
+array._isindex = function (arr, tind)
+   if #arr.size ~= #tind then return false end
+   for i = 1,#tind do 
+      if tind[i] > arr.size[i] or tind[i] < 1 then return false end
    end
    return true
 end
 
 -- Transform index into single dimension representation.
-array._iconvert = function (arr, ind)
-   local res, k = ind[1], arr.k
-   for i = 2,#ind do 
-      res = res + (ind[i]-1) * k[i]
+array._iconvert = function (arr, tind)
+   local res, k = tind[1], arr.k
+   for i = 2,#tind do 
+      res = res + (tind[i]-1) * k[i]
    end
    return res
 end
@@ -125,23 +126,23 @@ end
 
 --- Get array element.
 --    @param arr Array object.
---    @param ind Index of the element (table).
+--    @param tind Index of the element (table).
 --    @return Element or error if index is wrong.
-array.get = function (arr, ind)
-   assert(array._icorrect(arr, ind), "Wrong index!")
-   return arr[array._iconvert(arr, ind)]
+array.get = function (arr, tind)
+   assert(array._isindex(arr, tind), "Wrong index!")
+   return arr[array._iconvert(arr, tind)]
 end
-array.about[array.get] = {"get(arr,ind)", "Get array element. Index is a table.", }
+array.about[array.get] = {"get(arr,ind)", "Get array element. Index is a table."}
 
 --- Set new value.
 --    @param arr Array object.
---    @param ind Element index (table).
+--    @param tind Element index (table).
 --    @param val New value.
-array.set = function (arr, ind, val)
-   assert(array._icorrect(arr, ind), "Wrong index!")
-   arr[array._iconvert(arr,ind)] = val 
+array.set = function (arr, tind, val)
+   assert(array._isindex(arr, tind), "Wrong index!")
+   arr[array._iconvert(arr,tind)] = val 
 end
-array.about[array.set] = {"set(arr,ind,val)", "Set value to the array. Index is a table.", }
+array.about[array.set] = {"set(arr,ind,val)", "Set value to the array. Index is a table."}
 
 --- Get array copy.
 --    @param arr Array object.
@@ -156,10 +157,10 @@ array.about[array.copy] = {"copy(arr)", "Get copy of the array.", help.OTHER}
 --    @param a1 First array object.
 --    @param a2 Second array object.
 --    @return <code>true</code> if size is the same.
-array.isequal = function (a1, a2)
-   if isarray(a1) and isarray(a2) and #a1.size == #a2.size then
-      for i = 1, #a1.size do
-         if a1.size[i] ~= a2.size[i] then return false end
+array.isequal = function (arr1, arr2)
+   if isarray(arr1) and isarray(arr2) and #arr1.size == #arr2.size then
+      for i = 1, #arr1.size do
+         if arr1.size[i] ~= arr2.size[i] then return false end
       end
       return true
    end
@@ -172,12 +173,12 @@ array.about[array.isequal] = {"isequal(a1,a2)", "Check size equality.", help.OTH
 --    @param a2 Second array object.
 --    @param fn Function with 2 arguments.
 --    @return New array where each element is result of the function evaluation fn(a1[i],a2[i]).
-array.apply = function (a1, a2, fn)
-   assert(array.isequal(a1,a2), "Not compatible arrays!")
-   local res, v = array:new(Ver.move(a1.size, 1, #a1.size, 1, {}))    -- prepare empty copy
-   for i = 1, array._capacity(a1) do 
-      v = fn(a1[i] or 0, a2[i] or 0)             -- elements can be empty
-      if v ~= 0 then res[i] = v end              -- save nonzero values
+array.apply = function (arr1, arr2, ffun)
+   assert(array.isequal(arr1,arr2), "Not compatible arrays!")
+   local res, v = array:new(Ver.move(arr1.size, 1, #arr1.size, 1, {}))    -- prepare empty copy
+   for i = 1, array._capacity(arr1) do 
+      v = ffun(arr1[i] or 0, arr2[i] or 0)           -- elements can be empty
+      if v ~= 0 then res[i] = v end                  -- save nonzero values
    end
    return res
 end
@@ -187,11 +188,11 @@ array.about[array.apply] = {"apply(a1,a2,fn)", "Apply function of 2 arguments. R
 --    @param a Array object.
 --    @param fn Function with 1 argument.
 --    @return New array where each element is result of the function evaluation fn(a[i]).
-array.map = function (a, fn)
-   local res, v = array:new(Ver.move(a.size, 1, #a.size, 1, {}))
-   for i = 1, array._capacity(a) do
-      v = fn(a[i] or 0)                          -- elements can be empty
-      if v ~= 0 then res[i] = v end              -- save nonzero values
+array.map = function (arr, ffun)
+   local res, v = array:new(Ver.move(arr.size, 1, #arr.size, 1, {}))
+   for i = 1, array._capacity(arr) do
+      v = ffun(arr[i] or 0)                          -- elements can be empty
+      if v ~= 0 then res[i] = v end                  -- save nonzero values
    end
    return res
 end
@@ -228,23 +229,23 @@ array.__pow = function (a1, a2)
 end
 
 array.arithmetic = 'arithmetic'
-array.about[array.arithmetic] = {array.arithmetic, "a+b, a-b, a*b, a/b, -a, a^b", }
+array.about[array.arithmetic] = {array.arithmetic, "a+b, a-b, a*b, a/b, -a, a^b"}
 
 --- Random array generator.
 --    @param s Size table.
 --    @return Array of the given size with random numbers from 0 to 1.
-array.rand = function (s)
-   local arr = array:new(s)
+array.rand = function (tsize)
+   local arr = array:new(tsize)
    for i = 1, array._capacity(arr) do arr[i] = math.random() end
    return arr
 end
-array.about[array.rand] = {"rand(size)", "Return array with random numbers between 0 and 1.", }
+array.about[array.rand] = {"rand(size)", "Return array with random numbers between 0 and 1."}
 
 -- a1 == a2
-array.__eq = function (a1, a2)
-   if array.isequal(a1,a2) then
-      for i = 1, array._capacity(a1) do
-	 if (a1[i] or 0) ~= (a2[i] or 0) then return false end
+array.__eq = function (arr1, arr2)
+   if array.isequal(arr1,arr2) then
+      for i = 1, array._capacity(arr1) do
+	 if (arr1[i] or 0) ~= (arr2[i] or 0) then return false end
       end
       return true
    end
@@ -252,7 +253,7 @@ array.__eq = function (a1, a2)
 end
 
 array.comparison = 'comparison'
-array.about[array.comparison] = {array.comparison, "a == b, a ~= b", }
+array.about[array.comparison] = {array.comparison, "a == b, a ~= b"}
 
 --- Get array dimension.
 --    @param arr Array object.
@@ -260,23 +261,23 @@ array.about[array.comparison] = {array.comparison, "a == b, a ~= b", }
 array.dim = function (arr)
    return Ver.move(arr.size, 1, #arr.size, 1, {})
 end
-array.about[array.dim] = {"dim(arr)", "Return size of array.", }
+array.about[array.dim] = {"dim(arr)", "Return size of array."}
 
 --- Get part of array between two indexes.
 --    @param arr Array object.
---    @param ind1 Index of the lower bound.
---    @param ind2 Index of the upper bound.
+--    @param tind1 Index of the lower bound.
+--    @param tind2 Index of the upper bound.
 --    @return Array of elements restricted by the indexes.
-array.sub = function (arr, ind1, ind2)
+array.sub = function (arr, tind1, tind2)
    -- negative index means the last element
-   for i = 1, #ind2 do 
-      if ind2[i] < 0 then ind2[i] = ind2[i] + arr.size[i] + 1 end 
+   for i = 1, #tind2 do 
+      if tind2[i] < 0 then tind2[i] = tind2[i] + arr.size[i] + 1 end 
    end
-   assert(array._icorrect(arr, ind1) and array._icorrect(arr, ind2), "Wrong index!")
+   assert(array._isindex(arr, tind1) and array._isindex(arr, tind2), "Wrong index!")
    -- prepare tables
    local newsize, ind = {}, {}
-   for i = 1, #ind1 do 
-      newsize[i] = ind2[i] - ind1[i] + 1 
+   for i = 1, #tind1 do 
+      newsize[i] = tind2[i] - tind1[i] + 1 
       ind[i]     = 0
    end 
    local res = array:new(newsize)
@@ -286,47 +287,48 @@ array.sub = function (arr, ind1, ind2)
       -- calculate new temporary index
       for i = 1, #ind do 
          local tmp = math.modf(count/K[i]) % S[i]
-	 ind2[i] = ind1[i] + tmp
+	 tind2[i] = tind1[i] + tmp
 	 ind[i] = tmp + 1
       end
-      res[array._iconvert(res,ind)] = arr[array._iconvert(arr,ind2)]
+      res[array._iconvert(res,ind)] = arr[array._iconvert(arr,tind2)]
    end
    return res
 end
-array.about[array.sub] = {"sub(arr,ind1,ind2)", "Return subarray restricted by 2 indexes.", }
+array.about[array.sub] = {"sub(arr,ind1,ind2)", "Return subarray restricted by 2 indexes."}
 
 --- Concatenate 2 arrays along given axes.
 --    @param arr1 First array object.
 --    @param arr2 Second array object.
+--    @param naxis Axis number.
 --    @return New concatenated array.
-array.concat = function (arr1, arr2, axe)
+array.concat = function (arr1, arr2, naxis)
    -- check size
-   assert(axe > 0 and axe <= #arr1.size, "Wrong axe!")
+   assert(naxis > 0 and naxis <= #arr1.size, "Wrong naxis!")
    for i = 1, #arr1.size do 
-     if not (arr1.size[i] == arr2.size[i] or i == axe) then error("Different size!") end
+     if not (arr1.size[i] == arr2.size[i] or i == naxis) then error("Different size!") end
    end
    -- prepare new size
    local newsize = Ver.move(arr1.size, 1, #arr1.size, 1, {})
-   newsize[axe] = newsize[axe] + arr2.size[axe]
+   newsize[naxis] = newsize[naxis] + arr2.size[naxis]
    -- combine
-   local res, ind1, ind2 = array:new(newsize), Ver.move(newsize, 1, #newsize, 1, {}), Ver.move(newsize, 1, #newsize, 1, {})
-   local edge = arr1.size[axe]
+   local res, ind1, ind2 = array:new(newsize), {}, {}
+   local edge = arr1.size[naxis]
    local K, S = res.k, res.size
    for count = 1, array._capacity(res) do
       -- prepare index
-      for i = 1, #ind1 do
+      for i = 1, #newsize do
          ind1[i] = math.modf(count/K[i]) % S[i] + 1
 	 ind2[i] = ind1[i]
       end
       -- get value
-      local second = ind1[axe] > edge
-      ind2[axe] = second and (ind2[axe]-edge) or ind2[axe]
+      local second = ind1[naxis] > edge
+      ind2[naxis] = second and (ind2[naxis]-edge) or ind2[naxis]
       res[array._iconvert(res,ind1)] = second and arr2[array._iconvert(arr2,ind2)] or arr1[array._iconvert(arr1,ind2)]
    end
    --for i = 1, array._capacity(arr2) do print(i, arr2[i]) end
    return res 
 end
-array.about[array.concat] = {"concat(a1,a2,axe)", "Array concatenation along the given axis.", }
+array.about[array.concat] = {"concat(a1,a2,axe)", "Array concatenation along the given axis."}
 
 --- Method #
 --    @param arr Array object.
@@ -343,10 +345,10 @@ end
 --- Get array slice.
 --    Show sequence of 2D matrices with array elements.
 --    @param arr Array object.
---    @param r Number of axes for representation as rows.
---    @param c Number of axes for representation as columns.
+--    @param nrow Number of axes for representation as rows.
+--    @param ncol Number of axes for representation as columns.
 --    @return String with all array elements slice by slice.
-array.fullstring = function (arr, r, c)
+array.fullstring = function (arr, nrow, ncol)
    local res = {}
    local S = arr.size
    -- 1 dimensional array
@@ -354,19 +356,19 @@ array.fullstring = function (arr, r, c)
       for i = 1, S[1] do res[i] = arr[i] or 0 end
       return table.concat(res, ' ')
    elseif #S == 2 then
-      r = r or 1
-      c = c or 2
+      nrow = nrow or 1
+      ncol = ncol or 2
    end
-   assert(r > 0 and r <= #S and c > 0 and c <= #S and r ~= c, "Wrong indexes!")
+   assert(nrow > 0 and nrow <= #S and ncol > 0 and ncol <= #S and nrow ~= ncol, "Wrong indexes!")
    -- get 2D layer as string
    local function layer (ind)
             local row = {}
-	    for i = 1, S[r] do
-	       ind[r] = i
+	    for i = 1, S[nrow] do
+	       ind[nrow] = i
 	       -- get row
 	       local col = {}
-	       for j = 1, S[c] do
-	          ind[c]  = j
+	       for j = 1, S[ncol] do
+	          ind[ncol]  = j
 		  col[#col+1] = arr[array._iconvert(arr, ind)] or 0
 	       end
 	       -- save
@@ -378,7 +380,7 @@ array.fullstring = function (arr, r, c)
    local bound, current, extent = {}, {}, {}
    local prod = 1
    for i = 1, #S do
-      if i ~= r and i ~= c then 
+      if i ~= nrow and i ~= ncol then 
          bound[#bound+1] = S[i] 
 	 current[#current+1] = 0 
 	 extent[#extent+1] = prod
@@ -393,8 +395,8 @@ array.fullstring = function (arr, r, c)
       -- write
       local k = #current
       for i = #next_index, 1, -1 do
-         if     i == r then next_index[i] = 'R'
-	 elseif i == c then next_index[i] = 'C'
+         if     i == nrow then next_index[i] = 'R'
+	 elseif i == ncol then next_index[i] = 'C'
 	 else
 	    next_index[i] = current[k]+1
 	    k = k-1
