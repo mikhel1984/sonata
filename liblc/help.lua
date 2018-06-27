9bfb96455b1418cc39f686a149262eee13c1971f
Stas <mikhel.sk@gmail.com>
2018-06-27 19:21:51 +0300

    Refactoring in help module

--- liblc/help.lua
+++ liblc/help.lua
@@ -39,6 +39,8 @@ To use language localization, create text file with Lua table in format
 Use about:localization("file_name") to load it.
 --]]
 
+--	LOCAL
+
 -- directory with language files
 local LOCALE = 'locale'
 local LIB = 'liblc'
@@ -47,9 +49,7 @@ local TITLE, DESCRIPTION, CATEGORY, MODULE = 1, 2, 3, 4
 local MAIN = 1
 local _MAIN_ = '__main__'
 
---- English version of some interface strings.
---    @class table
---    @name eng
+-- English version of some interface strings.
 local eng = {
 intro = [[
 Print 'import(module)' to expand functionality.
@@ -66,166 +66,7 @@ Use
 to get additional modules.]],
 }
 
--------------------------------------------- 
--- @class table
--- @name help
--- @field BASE Constant.
--- @field TRIG Constant.
--- @field HYP Constant.
--- @field CONST Constant.
--- @field OTHER Constant.
--- @field NEW Constant.
--- @field SEP Current system separator.
-local help = {}
-help.__index = help
--- constant strings
-help.BASE = 'base'
-help.TRIG = 'trigonometry'
-help.HYP = 'hyperbolic'
-help.CONST = 'constants'
-help.OTHER = 'other'
-help.NEW = 'constructor'
-help.META = 'methods'
-
-help.SEP = string.sub(package.config,1,1)
-
-help.CMAIN = ''
-help.CHELP = ''
-help.CRESET = ''
-help.CBOLD = ''
-help.CNBOLD = ''
-
---- Create new object, set metatable.
---    @param str Module description.
---    @return Array object.
-function help:new(str)
-   assert(str and type(str) == 'string', "Constructor must include description!")
-   local o = {}
-   o[o] = {link=o, str}        -- save link to itself
-   setmetatable(o, self)
-   return o
-end
-
---- Create list of functions, sort by module and category.
---    <i>Private function.</i>
---    @param tbl Table with descriptions.
---    @return Sorted table.
-local function funclist(tbl)
-   local res = {}
-   for k, v in pairs(tbl) do
-      -- only main description contains 'link'
-      if not v.link then
-         local category = v[CATEGORY] or help.BASE
-	 local module = v[MODULE] or "Default"
-	 res[module] = res[module] or {}                      -- create table for each module
-         res[module][category] = res[module][category] or {}  -- add table for each category
-         table.insert(res[module][category], v[TITLE])        -- insert function file into this table
-      end
-   end
-   return res
-end
-
---- Print information about function or list of all possible functions.
---    @param fn Function or module for getting manual.
-function help:print(fn)
-   io.write(help.CHELP)
-   if fn then
-      -- expected module or function description
-      local v = self[fn]
-      if not v then print('No help for :',fn); return end
-      if v.link then                  
-         -- module common description
-         print('\n'..v[MAIN], '\n')
-	 -- details
-	 return v.link:print()
-      else                           
-         -- function description
-         print(string.format("  :%s\n%s", v[TITLE], v[DESCRIPTION]))
-      end
-   else                               
-      -- sort functions
-      local lst = funclist(self)
-      for mod, t in pairs(lst) do                   -- for each module
-         io.write(help.CBOLD)
-         print(string.format("\t%s", mod))
-	 io.write(help.CNBOLD)
-         for cat, n in pairs(t) do                  -- for each category
-            print(string.format("  /%s", cat))
-	    for i, v in ipairs(n) do                -- for each function
-	       io.write(v, (i ~= #n and ', ' or ''))
-	    end
-	    print()                   -- new line
-         end
-	 print()
-      end
-   end
-end
-
-function help.usecolors(use)
-   if use then
-      help.CMAIN = '\x1B[32m' 
-      help.CHELP = '\x1B[33m' 
-      help.CRESET = '\x1B[0m'
-      help.CBOLD = '\x1B[1m'
-      help.CNBOLD = '\x1B[22m'
-   end
-end
-
---- Include content of the other help table into current one.
---    @param tbl Table to add.
---    @param nm Name of the added module.
-function help:add(tbl, nm)
-   assert(nm, "Module name is required!")
-   -- localization data
-   local mt = getmetatable(self)
-   local lng = mt.locale and mt.locale[nm]
-   -- prepare new 
-   for k, v in pairs(tbl) do 
-      if not v.link then v[MODULE] = nm end -- function description doesn't contain 'link' element
-      -- set localization
-      if lng then
-         if v.link then
-	    -- common description
-	    v[MAIN] = lng[_MAIN_] or v[MAIN]
-	 else
-	    -- details
-            v[DESCRIPTION] = lng[v[TITLE]] or v[DESCRIPTION]
-	    v[CATEGORY] = self:get(v[CATEGORY])
-	 end
-      end
-      self[k] = v                                -- add to base description table 
-   end
-   if lng then mt.locale[nm] = nil end -- free memory
-end
-
---- Read file with localization data and update main module.
---    @param fname Name of the file with translated text.
-function help:localization(fname)
-   fname = LOCALE..help.SEP..fname
-   -- call method of the 'files' module
-   help.lc_files = help.lc_files or require('liblc.files')
-   local lng = help.lc_files.tblimport(fname)
-   if lng then
-      getmetatable(self).locale = lng           
-      -- update functions in calc.lua
-      local lc = lng.Main
-      for k,v in pairs(self) do
-         if v.link then
-	    -- common description
-	    self[k][MAIN] = lc[_MAIN_] or v[MAIN]
-	 else
-	    -- details
-	    self[k][DESCRIPTION] = lc[v[TITLE]] or v[DESCRIPTION]
-	    self[k][CATEGORY] = self:get(v[CATEGORY])
-	 end
-      end
-   else
-      print("File " .. fname .. " wasn't found.")
-   end
-end
-
---- Add table 'about' into the 'eng' for saving into localization file.
---    <i>Private function.</i>
+-- Add table 'about' into the 'eng' for saving into localization file.
 local function eng2about()
    eng.about = {}
    for k,v in pairs(eng) do
@@ -234,8 +75,7 @@ local function eng2about()
    eng.about.about = nil
 end
 
---- Prepare strings with help information of the given module.
---    <i>Private function.</i>
+-- Prepare strings with help information of the given module.
 --    @param module Module name or table.
 --    @param alias Alias of the module name.
 --    @param lng Localization table from existing file.
@@ -273,10 +113,158 @@ local function helplines(module, alias, lng)
    return table.concat(res, '\n')
 end
 
+--	MODULE
+
+local help = {
+-- constant strings
+BASE = 'base',
+TRIG = 'trigonometry',
+HYP = 'hyperbolic',
+CONST = 'constants',
+OTHER = 'other',
+NEW = 'constructor',
+META = 'methods',
+-- file name separator
+SEP = string.sub(package.config,1,1),
+-- colors
+CMAIN = '',
+CHELP = '',
+CRESET = '',
+CBOLD = '',
+CNBOLD = '',
+}
+-- metamethods
+help.__index = help
+
+--- Create new object, set metatable.
+--    @param str Module description.
+--    @return Array object.
+help.new = function (self,str)
+   assert(str and type(str) == 'string', "Constructor must include description!")
+   local o = {}
+   o[o] = {link=o, str}        -- save link to itself
+   return setmetatable(o, self)
+end
+
+-- Create list of functions, sort by module and category.
+help._funclist = function (tbl)
+   local res = {}
+   for k, v in pairs(tbl) do
+      -- only main description contains 'link'
+      if not v.link then
+         local category = v[CATEGORY] or help.BASE
+	 local module = v[MODULE] or "Default"
+	 res[module] = res[module] or {}                      -- create table for each module
+         res[module][category] = res[module][category] or {}  -- add table for each category
+         table.insert(res[module][category], v[TITLE])        -- insert function file into this table
+      end
+   end
+   return res
+end
+
+--- Print information about function or list of all possible functions.
+--    @param fn Function or module for getting manual.
+help.print = function (self,fn)
+   io.write(help.CHELP)
+   if fn then
+      -- expected module or function description
+      local v = self[fn]
+      if not v then print('No help for :',fn); return end
+      if v.link then                  
+         -- module common description
+         print('\n'..v[MAIN], '\n')
+	 -- details
+	 return v.link:print()
+      else                           
+         -- function description
+         print(string.format("  :%s\n%s", v[TITLE], v[DESCRIPTION]))
+      end
+   else                               
+      -- sort functions
+      local lst = help._funclist(self)
+      for mod, t in pairs(lst) do                   -- for each module
+         io.write(help.CBOLD)
+         print(string.format("\t%s", mod))
+	 io.write(help.CNBOLD)
+         for cat, n in pairs(t) do                  -- for each category
+            print(string.format("  /%s", cat))
+	    for i, v in ipairs(n) do                -- for each function
+	       io.write(v, (i ~= #n and ', ' or ''))
+	    end
+	    print()                   -- new line
+         end -- for
+	 print()
+      end -- for
+   end -- if
+end
+
+help.usecolors = function (use)
+   if use then
+      help.CMAIN = '\x1B[32m' 
+      help.CHELP = '\x1B[33m' 
+      help.CRESET = '\x1B[0m'
+      help.CBOLD = '\x1B[1m'
+      help.CNBOLD = '\x1B[22m'
+   end
+end
+
+--- Include content of the other help table into current one.
+--    @param tbl Table to add.
+--    @param nm Name of the added module.
+help.add = function (self,tbl,nm)
+   assert(nm, "Module name is required!")
+   -- localization data
+   local mt = getmetatable(self)
+   local lng = mt.locale and mt.locale[nm]
+   -- prepare new 
+   for k, v in pairs(tbl) do 
+      if not v.link then v[MODULE] = nm end      -- function description doesn't contain 'link' element
+      if lng then
+         -- set localization
+         if v.link then
+	    -- common description
+	    v[MAIN] = lng[_MAIN_] or v[MAIN]
+	 else
+	    -- details
+            v[DESCRIPTION] = lng[v[TITLE]] or v[DESCRIPTION]
+	    v[CATEGORY] = self:get(v[CATEGORY])
+	 end -- if
+      end 
+      self[k] = v                                -- add to base description table 
+   end
+   if lng then mt.locale[nm] = nil end -- free memory
+end
+
+--- Read file with localization data and update main module.
+--    @param fname Name of the file with translated text.
+help.localization = function (self,fname)
+   fname = LOCALE..help.SEP..fname
+   -- call method of the 'files' module
+   help.lc_files = help.lc_files or require('liblc.files')
+   local lng = help.lc_files.tblimport(fname)
+   if lng then
+      getmetatable(self).locale = lng           
+      -- update functions in calc.lua
+      local lc = lng.Main
+      for k,v in pairs(self) do
+         if v.link then
+	    -- common description
+	    self[k][MAIN] = lc[_MAIN_] or v[MAIN]
+	 else
+	    -- details
+	    self[k][DESCRIPTION] = lc[v[TITLE]] or v[DESCRIPTION]
+	    self[k][CATEGORY] = self:get(v[CATEGORY])
+	 end
+      end
+   else
+      print("File " .. fname .. " wasn't found.")
+   end
+end
+
 --- Prepare and save localization data.
 --    @param fname Language name, for example 'en' or 'it'.
 --    @param modules Table with the list of existing modules.
-function help.prepare(fname, modules)
+help.prepare = function(fname, modules)
    fname = string.format('%s%s%s.lng', LOCALE, help.SEP, fname)
    -- call method of the 'files' module
    help.lc_files = help.lc_files or require('liblc.files')
@@ -306,7 +294,7 @@ end
 --- Get translated string if possible.
 --    @param txt Text to seek.
 --    @return Translated or initial text.
-function help:get(txt)
+help.get = function (self,txt)
    local mt = getmetatable(self)
    local lng = mt.locale and mt.locale.Dialog and mt.locale.Dialog[txt]  -- check in localization table
    return lng or eng[txt] or txt
@@ -316,16 +304,16 @@ end
 --    @param mname Module name.
 --    @param alias Module short name.
 --    @param description Short module description.
-function help.newmodule (mname, alias, description)
+help.newmodule = function (mname, alias, description)
    if not (mname and alias) then
-      print('Both module name and alias are expected!'); return
+      error('Both module name and alias are expected!')
    end
    local fname = string.format('%s%s%s.lua', LIB, help.SEP, mname)
    -- check existence
    local f = io.open(fname) 
    if f then 
       f:close()
-      print('File '..fname..' is already exist!'); return
+      print('File '..fname..' is already exists!'); return
    end
    -- write new file
    description = description or "This is my cool module!"
