31ea7a124fa8bfbb3198bdf7a3cf8383170c81ad
Stanislav <mikhel.sk@gmail.com>
2021-05-12 15:19:20 +0300

    Change version

--- sonata.lua
+++ sonata.lua
@@ -19,7 +19,7 @@
 --=====================  CODE  ========================
 
 -- Environment
-lc_local = { version = '0.9.17' }
+lc_local = { version = '0.9.18' }
 
 -- Add path to the libraries
 if LC_ADD_PATH then
