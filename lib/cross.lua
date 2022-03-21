017d00b69a060f0ebab3647bea572f6f7362a12e
Stanislav <mikhel.sk@gmail.com>
2022-03-21 20:49:37 +0300

    Add tests for rat + poly

--- lib/cross.lua
+++ lib/cross.lua
@@ -47,7 +47,28 @@ print(a)
 Poly = require 'lib.polynomial'
 
 -- check '%' operation
-ans = Rat.gcd(Poly{2,7,6}, Poly{3,10,8})  --> Poly{1,2}
+ans = Rat.gcd(Poly{2,7,6}, Poly{3,10,8})  --> Poly{-0.5,-1}
+
+a = Rat(Poly{1,2,1}, Poly{2,3})
+b = Rat(Poly{2,3})
+ans = (a ~= nil) and (b ~= nil)  --> true
+
+ans = a + b                      --> Rat(Poly{20,56,40},Poly{8,12})
+
+ans = a * b                      --> Poly{1,2,1}
+
+ans = a + 1                      --> Rat(Poly{1,4,4},Poly{2,3})
+
+a = Rat(Poly{1,1},Poly{1,2}) 
+ans = a ^ 2                      --> a * a
+
+d = a:copy() 
+ans = (a:Nu().type == d:Nu().type)  --> true
+
+a = Poly{1,2,3}
+ans = Rat(5*a, 5)                   --> a
+
+
 
 
 
