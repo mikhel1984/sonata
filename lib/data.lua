84a4c1e53bdca1782fd823c70e0afe8307f69656
Stanislav <mikhel.sk@gmail.com>
2022-05-24 20:37:22 +0300

    Sort methods in 'data'

--- lib/data.lua
+++ lib/data.lua
@@ -68,11 +68,11 @@ ans = Data.harmmean(X,W)     --3> 2.567
 
 -- find histogram
 a,b = Data.histcounts(X, 3)
-ans = b[1]                    --> 1
+ans = b[1]                    --> 2.25
 
 -- define edges 
-a,b = Data.histcounts(X,{0,4,7})  
-ans = a[1]                    --> 5
+a,b = Data.histcounts(X,{2,4,7})
+ans = a[1]                    --> 1
 
 -- table range reference 
 a = Data.ref(X, 3, 6)
@@ -125,215 +125,6 @@ local about = help:new("Data processing and statistics.")
 
 local data = {}
 
---- Sum of all elements.
---  @param t Table with numbers.
---  @return Sum.
-data.sum = function (t)
-  local s = 0
-  for i = 1, #t do s = s+t[i] end
-  return s
-end
-about[data.sum] = {"sum(t)", "Get sum of all elements.", help.OTHER}
-
---- Average value.
---  @param t Table with numbers.
---  @param tw Table with weight. Can be omitted.
---  @return Average.
-data.mean = function (t, tw)
-  if tw then
-    local st, sw = 0, 0
-    for i = 1, #t do
-      local w = tw[i] or 1
-      st = st + t[i]*w
-      sw = sw + w
-    end
-    return st / sw
-  else
-    return data.sum(t) / #t
-  end
-end
-about[data.mean] = {"mean(t,[tw])", "Calculate average value. Weights can be used.", STAT}
-
---- Standard deviation and variance.
---  @param t Table of numbers.
---  @param tw Table of weights.
---  @return Standard deviation, variance.
-data.std = function (t, tw)
-  local mean = data.mean(t,tw)
-  local disp = 0
-  if tw then
-    local sw = 0
-    for i = 1,#t do
-      local w = tw[i] or 1
-      disp = disp + w*(t[i]-mean)^2 
-      sw = sw + w
-    end
-    disp = disp / sw
-  else
-    for i = 1,#t do disp = disp + (t[i]-mean)^2 end
-    disp = disp / #t
-  end
-  return math.sqrt(disp), disp 
-end
-about[data.std] = {"std(t,[tw])", "Standard deviation and variance. Weights can be used.", STAT}
-
---- Maximum value.
---  @param t Table of numbers.
---  @return Maximum value and its index.
-data.max = function (t)
-  local m, k = t[1], 1
-  for i = 2, #t do
-    if t[i] > m then m, k = t[i], i end
-  end
-  return m,k
-end
-about[data.max] = {"max(t)", "Maximal element and its index.", help.OTHER}
-
---- Minimum value.
---  @param t Table of numbers.
---  @return Minimum value and its index.
-data.min = function (t)
-  local m, k = t[1], 1
-  for i = 2, #t do
-    if t[i] < m then m, k = t[i], i end
-  end
-  return m,k
-end
-about[data.min] = {"min(t)", "Minimal element and its index.", help.OTHER}
-
---- Geometrical mean.
---  @param t Table of numbers.
---  @param tw Table of weights, optional.
---  @return Geometrical mean.
-data.geomean = function (t, tw)
-  if tw then
-    local st, sw = 0, 0
-    for i = 1,#t do
-      local w = tw[i] or 1
-      st = st + w*math.log(t[i])
-      sw = sw + w
-    end
-    return math.exp(st / sw)
-  else
-    local p = 1
-    for i = 1, #t do p = p * t[i] end
-    return p^(1/#t)
-  end
-end
-about[data.geomean] = {"geomean(t,[tw])", "Geometrical mean.", STAT}
-
---- Harmonic mean.
---  @param t Table of numbers.
---  @param tw Table of weights. Can be omitted.
---  @return Harmonic mean.
-data.harmmean = function (t, tw)
-  if tw then
-    local st, sw = 0, 0
-    for i = 1,#t do
-      local w = tw[i]
-      st = st + w/t[i]
-      sw = sw + w
-    end
-    return sw / st
-  else
-    local h = 0
-    for i = 1, #t do h = h + 1/t[i] end
-    return #t / h
-  end
-end
-about[data.harmmean] = {"harmmean(t,[tw])", "Harmonic mean.", STAT}
-
---- Find median.
---  @param t Table of numbers.
---  @return Value of median.
-data.median = function (t)
-  local len = #t
-  local y = Ver.move(t,1,len,1,{})
-  table.sort(y)
-  if len % 2 == 1 then 
-    return y[(len+1)/2]
-  else
-    len = len / 2
-    return (y[len] + y[len+1]) * 0.5
-  end
-end
-about[data.median] = {"median(t)", "Median of the list.", STAT}
-
---- Frequency of elements.
---  @param t Table of numbers.
---  @return Table where keys are elements and values are their frequencies.
-data.freq = function (t)
-  local tmp = {}
-  for _, v in ipairs(t) do
-    tmp[v] = (tmp[v] or 0) + 1
-  end
-  return tmp
-end
-about[data.freq] = {"freq(t)", "Return table with frequencies of elements.", STAT}
-
---- Central moment.
---  @param N Order of the moment.
---  @param t Table of numbers.
---  @param tw Table of weights. Can be omitted.
---  @return Central moment value.
-data.moment = function (N, t, tw)
-  local m, n = 0, 0
-  for i = 1,#t do
-    local w = tw and tw[i] or 1
-    m = m + w * t[i]
-    n = n + w
-  end
-  m = m / n
-  local mu = 0
-  for i = 1,#t do
-    mu = mu + (tw and tw[i] or 1) * (t[i]-m)^N
-  end
-  return mu / n
-end
-about[data.moment] = {"moment(N,t,[tw])", "Central moment of t order N, tw is a list of weights.", STAT}
-
---- Number of elements in each bin.
---  @param t Data table.
---  @param rng Number of bins or table with edges.
---  @return Two tables, with sum and edges.
-data.histcounts = function (t, rng)
-  rng = rng or 10 
-  local bins
-  -- make copy and sort
-  local y = Ver.move(t,1,#t,1,{})
-  table.sort(y)
-  -- prepare edges
-  if type(rng) == 'number' then
-    local vMin, vMax = y[1], y[#y]
-    local wid = (vMax - vMin) / (rng - 1)
-    bins = {}
-    for v = vMin + 0.5*wid, vMax, wid do bins[#bins+1] = v end  
-  elseif type(rng) == 'table' then    
-    bins = rng
-  else
-    error("Expected number or table")
-  end
-  -- check order
-  for i = 2,#bins do 
-    if bins[i] <= bins[i-1] then error("Wrong order") end
-  end  
-  -- fill result
-  local res = {}
-  for i = 1,#bins+1 do res[i] = 0 end
-  local p, i = 1, 1 
-  while i <= #y do
-    local v = y[i]
-    if p > #bins or v < bins[p] then
-        res[p] = res[p] + 1
-        i = i + 1
-    else
-      p = p + 1
-    end
-  end
-  return res, bins
-end
-about[data.histcounts] = {"histcounts(X,[rng=10])","Calculate amount of bins. Edges can be either number or table.", STAT}
-
 --- Estimate covariance for two vectors.
 --  @param t1 First data vector.
 --  @param t2 Second data vector.
@@ -369,29 +160,6 @@ data.cov = function (t)
   return m
 end
 about[data.cov] = {"cov(t)", "Find covariance matrix for list of vectors.", STAT}
-
-
---- Student's cumulative distribution
---   @param d Value.
---   @param N Degree of freedom.
---   @return Cumulative value.
-data.tcdf = function (d,N)
-  data.ext_special = data.ext_special or require('lib.special')
-  local tmp = N/(N+d*d)
-  return 1-0.5*data.ext_special.betainc(tmp,0.5*N,0.5)
-end
-about[data.tcdf] = {"tcdf(d,N)", "Student's cumulative distribution.", help.OTHER}
-
---- Student's density function.
---   @param d Value.
---   @param N Degree of freedom.
---   @return Density value.
-data.tpdf = function (d,N)
-  data.ext_special = data.ext_special or require('lib.special')
-  local tmp = math.sqrt(N)*data.ext_special.beta(0.5,0.5*N)
-  return (1+d*d/N)^(-0.5*(N+1))/tmp
-end
-about[data.tpdf] = {"tpdf(d,N)", "Student's distribution density.", help.OTHER}
 
 --- Save Lua table in file, use given delimiter.
 --  @param sFile File name.
@@ -462,6 +230,20 @@ data.csvread = function (sFile, char, bCol)
 end
 about[data.csvread] = {"csvread(sFile,[delim=',',bCol=false])", "Read delimiter separated data as Lua table.", FILES}
 
+--- Generate function from string.
+--  @param sExpr Expression for execution.
+--  @param iArg Number of arguments (optional).
+--  @return Function based on the expression.
+data.F = function (sExpr,iArg)
+  local arg = {}
+  for i = 1, (iArg or 2) do arg[i] = string.format("x%d", i) end
+  local fn = Ver.loadStr(
+    string.format("return function (%s) return %s end", table.concat(arg,','), sExpr)
+  )
+  return fn()
+end
+about[data.F] = {"F(sExpr,[iArg=2])", "Generate function from expression of x1, x2 etc.", help.OTHER}
+
 --- Find elements using condition.
 --  @param t Table with data.
 --  @param vCond Either boolean function or table of weights.
@@ -483,6 +265,102 @@ data.filter = function (t, vCond)
   return res
 end
 about[data.filter] = {"filter(t,vCond)","Get result of the table filtering. Condition is either boolean function or table of weights.", FILTER}
+
+--- Frequency of elements.
+--  @param t Table of numbers.
+--  @return Table where keys are elements and values are their frequencies.
+data.freq = function (t)
+  local tmp = {}
+  for _, v in ipairs(t) do
+    tmp[v] = (tmp[v] or 0) + 1
+  end
+  return tmp
+end
+about[data.freq] = {"freq(t)", "Return table with frequencies of elements.", STAT}
+
+--- Geometrical mean.
+--  @param t Table of numbers.
+--  @param tw Table of weights, optional.
+--  @return Geometrical mean.
+data.geomean = function (t, tw)
+  if tw then
+    local st, sw = 0, 0
+    for i = 1,#t do
+      local w = tw[i] or 1
+      st = st + w*math.log(t[i])
+      sw = sw + w
+    end
+    return math.exp(st / sw)
+  else
+    local p = 1
+    for i = 1, #t do p = p * t[i] end
+    return p^(1/#t)
+  end
+end
+about[data.geomean] = {"geomean(t,[tw])", "Geometrical mean.", STAT}
+
+--- Harmonic mean.
+--  @param t Table of numbers.
+--  @param tw Table of weights. Can be omitted.
+--  @return Harmonic mean.
+data.harmmean = function (t, tw)
+  if tw then
+    local st, sw = 0, 0
+    for i = 1,#t do
+      local w = tw[i]
+      st = st + w/t[i]
+      sw = sw + w
+    end
+    return sw / st
+  else
+    local h = 0
+    for i = 1, #t do h = h + 1/t[i] end
+    return #t / h
+  end
+end
+about[data.harmmean] = {"harmmean(t,[tw])", "Harmonic mean.", STAT}
+
+--- Number of elements in each bin.
+--  @param t Data table.
+--  @param rng Number of bins or table with edges.
+--  @return Two tables, with sum and edges.
+data.histcounts = function (t, rng)
+  rng = rng or 10 
+  local bins
+  -- make copy and sort
+  local y = Ver.move(t,1,#t,1,{})
+  table.sort(y)
+  -- prepare edges
+  if type(rng) == 'number' then
+    local vMin, vMax = y[1], y[#y]
+    local wid = (vMax - vMin) / (rng - 1)
+    bins = {}
+    for v = vMin + 0.5*wid, vMax, wid do bins[#bins+1] = v end
+  elseif type(rng) == 'table' then    
+    bins = rng
+  else
+    error("Expected number or table")
+  end
+  -- check order
+  for i = 2,#bins do 
+    if bins[i] <= bins[i-1] then error("Wrong order") end
+  end  
+  -- fill result
+  local res = {}
+  for i = 1,#bins+1 do res[i] = 0 end
+  local p, i = 1, 1 
+  while i <= #y do
+    local v = y[i]
+    if p > #bins or v < bins[p] then
+        res[p] = res[p] + 1
+        i = i + 1
+    else
+      p = p + 1
+    end
+  end
+  return res, bins
+end
+about[data.histcounts] = {"histcounts(X,[rng=10])","Calculate amount of bins. Edges can be either number or table.", STAT}
 
 --- Find weights (1/0) based on condition.
 --  @param t Data table.
@@ -510,13 +388,149 @@ data.isNot = function (t, fn)
 end
 about[data.isNot] = {"isNot(t,fn)", "Find inverted weights using boolean function.", FILTER}
 
---- Condition: x < d
---  @param d Upper limit. 
---  @return Boolean function.
-data.xLt = function (d)
-  return (function (x) return x < d end)
+--- Maximum value.
+--  @param t Table of numbers.
+--  @return Maximum value and its index.
+data.max = function (t)
+  local m, k = t[1], 1
+  for i = 2, #t do
+    if t[i] > m then m, k = t[i], i end
+  end
+  return m,k
 end
-about[data.xLt] = {"xLt(d)", "Return function for condition x < d.", FILTER}
+about[data.max] = {"max(t)", "Maximal element and its index.", help.OTHER}
+
+--- Average value.
+--  @param t Table with numbers.
+--  @param tw Table with weight. Can be omitted.
+--  @return Average.
+data.mean = function (t, tw)
+  if tw then
+    local st, sw = 0, 0
+    for i = 1, #t do
+      local w = tw[i] or 1
+      st = st + t[i]*w
+      sw = sw + w
+    end
+    return st / sw
+  else
+    return data.sum(t) / #t
+  end
+end
+about[data.mean] = {"mean(t,[tw])", "Calculate average value. Weights can be used.", STAT}
+
+--- Find median.
+--  @param t Table of numbers.
+--  @return Value of median.
+data.median = function (t)
+  local len = #t
+  local y = Ver.move(t,1,len,1,{})
+  table.sort(y)
+  if len % 2 == 1 then 
+    return y[(len+1)/2]
+  else
+    len = len / 2
+    return (y[len] + y[len+1]) * 0.5
+  end
+end
+about[data.median] = {"median(t)", "Median of the list.", STAT}
+
+--- Minimum value.
+--  @param t Table of numbers.
+--  @return Minimum value and its index.
+data.min = function (t)
+  local m, k = t[1], 1
+  for i = 2, #t do
+    if t[i] < m then m, k = t[i], i end
+  end
+  return m,k
+end
+about[data.min] = {"min(t)", "Minimal element and its index.", help.OTHER}
+
+--- Central moment.
+--  @param N Order of the moment.
+--  @param t Table of numbers.
+--  @param tw Table of weights. Can be omitted.
+--  @return Central moment value.
+data.moment = function (N, t, tw)
+  local m, n = 0, 0
+  for i = 1,#t do
+    local w = tw and tw[i] or 1
+    m = m + w * t[i]
+    n = n + w
+  end
+  m = m / n
+  local mu = 0
+  for i = 1,#t do
+    mu = mu + (tw and tw[i] or 1) * (t[i]-m)^N
+  end
+  return mu / n
+end
+about[data.moment] = {"moment(N,t,[tw])", "Central moment of t order N, tw is a list of weights.", STAT}
+
+
+--- Sum of all elements.
+--  @param t Table with numbers.
+--  @return Sum.
+data.sum = function (t)
+  local s = 0
+  for i = 1, #t do s = s+t[i] end
+  return s
+end
+about[data.sum] = {"sum(t)", "Get sum of all elements.", help.OTHER}
+
+--- Standard deviation and variance.
+--  @param t Table of numbers.
+--  @param tw Table of weights.
+--  @return Standard deviation, variance.
+data.std = function (t, tw)
+  local mean = data.mean(t,tw)
+  local disp = 0
+  if tw then
+    local sw = 0
+    for i = 1,#t do
+      local w = tw[i] or 1
+      disp = disp + w*(t[i]-mean)^2 
+      sw = sw + w
+    end
+    disp = disp / sw
+  else
+    for i = 1,#t do disp = disp + (t[i]-mean)^2 end
+    disp = disp / #t
+  end
+  return math.sqrt(disp), disp 
+end
+about[data.std] = {"std(t,[tw])", "Standard deviation and variance. Weights can be used.", STAT}
+
+--- Student's cumulative distribution
+--   @param d Value.
+--   @param N Degree of freedom.
+--   @return Cumulative value.
+data.tcdf = function (d,N)
+  data.ext_special = data.ext_special or require('lib.special')
+  local tmp = N/(N+d*d)
+  return 1-0.5*data.ext_special.betainc(tmp,0.5*N,0.5)
+end
+about[data.tcdf] = {"tcdf(d,N)", "Student's cumulative distribution.", help.OTHER}
+
+--- Student's density function.
+--   @param d Value.
+--   @param N Degree of freedom.
+--   @return Density value.
+data.tpdf = function (d,N)
+  data.ext_special = data.ext_special or require('lib.special')
+  local tmp = math.sqrt(N)*data.ext_special.beta(0.5,0.5*N)
+  return (1+d*d/N)^(-0.5*(N+1))/tmp
+end
+about[data.tpdf] = {"tpdf(d,N)", "Student's distribution density.", help.OTHER}
+
+--- Condition: x == d.
+--  @param d Some value.
+--  @return Boolean function.
+data.xEq = function (d)
+  return (function (x) return Cross.eq(x,d) end)
+end
+about[data.xEq] = {"xEq(d)", "Return function for condition x == d.", FILTER}
 
 --- Condition: x > d
 --  @param d Lower limit.
@@ -535,13 +549,40 @@ data.xIn = function (d1, d2)
 end
 about[data.xIn] = {"xIn(d1,d2)", "Return function for condition d1 <= x <= d2.", FILTER}
 
---- Condition: x == d.
---  @param d Some value.
+--- Condition: x < d
+--  @param d Upper limit. 
 --  @return Boolean function.
-data.xEq = function (d)
-  return (function (x) return Cross.eq(x,d) end)
+data.xLt = function (d)
+  return (function (x) return x < d end)
 end
-about[data.xEq] = {"xEq(d)", "Return function for condition x == d.", FILTER}
+about[data.xLt] = {"xLt(d)", "Return function for condition x < d.", FILTER}
+
+--- Apply function of n arguments to n lists.
+--  @param fn Function of multiple arguments.
+--  @param ... Sequence of lists.
+--  @return List of values fn(...).
+data.zip = function (fn, ...)
+  local ag, res = {...}, {}
+  local x, stop = {}, false
+  for i = 1, math.huge do
+    for j = 1, #ag do
+      local v = ag[j][i]
+      if v ~= nil then
+        x[j] = v
+      else 
+        stop = true
+        break 
+      end
+    end
+    if not stop then 
+      res[i] = fn(Ver.unpack(x))
+    else 
+      break 
+    end
+  end
+  return res 
+end
+about[data.zip] = {"zip(fn,...)", "Sequentially apply function to list of tables.", help.OTHER}
 
 -- Get reference to data range in other table
 local metaref = { type = 'ref' }
@@ -584,47 +625,6 @@ data.ref = function (t, iBeg, iEnd)
   return setmetatable({_beg=iBeg-1, _end=iEnd, _t=t}, metaref)
 end
 about[data.ref] = {'ref(t,[iBeg=1,iEnd=#t])', 'Return reference to the range of elements.', help.OTHER}
-
---- Apply function of n arguments to n lists.
---  @param fn Function of multiple arguments.
---  @param ... Sequence of lists.
---  @return List of values fn(...).
-data.zip = function (fn, ...)
-  local ag, res = {...}, {}
-  local x, stop = {}, false
-  for i = 1, math.huge do
-    for j = 1, #ag do
-      local v = ag[j][i]
-      if v ~= nil then
-        x[j] = v
-      else 
-        stop = true
-        break 
-      end
-    end
-    if not stop then 
-      res[i] = fn(Ver.unpack(x))
-    else 
-      break 
-    end
-  end
-  return res 
-end
-about[data.zip] = {"zip(fn,...)", "Sequentially apply function to list of tables.", help.OTHER}
-
---- Generate function from string.
---  @param sExpr Expression for execution.
---  @param iArg Number of arguments (optional).
---  @return Function based on the expression.
-data.F = function (sExpr,iArg)
-  local arg = {}
-  for i = 1, (iArg or 2) do arg[i] = string.format("x%d", i) end
-  local fn = Ver.loadStr(
-    string.format("return function (%s) return %s end", table.concat(arg,','), sExpr)
-  )
-  return fn()
-end
-about[data.F] = {"F(sExpr,[iArg=2])", "Generate function from expression of x1, x2 etc.", help.OTHER}
 
 -- Comment to remove descriptions
 data.about = about
