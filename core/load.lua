a14db318a946fe5f98052b3f54646ae307cf72b6
Stanislav <mikhel.sk@gmail.com>
2022-06-03 21:03:27 +0300

    Update docs

--- core/load.lua
+++ core/load.lua
@@ -16,7 +16,7 @@ SonataHelp.useColors(SONATA_USE_COLOR)
 
 -- Command evaluation.
 Sonata = require('core.evaluate')
-Sonata.version = '0.9.29'
+Sonata.version = '0.9.30'
 
 -- Quit the program
 quit = Sonata.exit
