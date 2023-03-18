879e7c31882dc21378cf44db0f92f854d51a18ce
Stanislav <mikhel.sk@gmail.com>
2023-03-18 17:30:26 +0300

    Add different distributions

--- lib/random.lua
+++ lib/random.lua
@@ -105,20 +105,136 @@ end
 about[random.new] = {":new() --> R", "Create generator object."}
 
 random.norm = function (R, dMean, dev)
-  dMean = dMean or 0
-  dev = dev or 1
+  dMean, dev = dMean or 0.0, dev or 1.0
   -- use Box-Muller transform
   local u, v, s = 0, 0, 0
   repeat
     u = 2*R:_fn()-1
     v = 2*R:_fn()-1
     s = u*u + v*v
-  until s <= 1 and s > 0
+  until 0 < s and s <= 1
   local norm = u * math.sqrt(-2*math.log(s)/s)
   return norm * dev + dMean
 end
 about[random.norm] = {":norm(mean_d=0, dev_d=1) --> float",
   "Normal distributed random value with the given mean and deviation."}
+
+random.exp = function (R, dLam)
+  local s = 0
+  repeat s = R:_fn() until s > 0
+  return -math.log(s) / (dLam or 1.0)
+end
+
+random.logistic = function (R, dMu, dSigma)
+  dMu, dSigma = dMu or 0.0, dSigma or 1.0
+  local s = 0
+  repeat s = R:_fn() until 0 < s and s < 1
+  return dMu + 0.551328895421792050*dSigma*math.log(s /(1.0 - s))
+end
+
+random.rayleigh = function (R)
+  local s = 0
+  repeat s = R:_fn() until 0 < s and s < 1
+  return math.sqrt(-2*math.log(s))
+end
+
+random.cauchy = function (R, dMu, dSigma)
+  dMu, dSigma = dMu or 0.0, dSigma or 1.0
+  local v1, v2 = 0, 0
+  repeat
+    v1 = 2*R:_fn() - 1
+    v2 = R:_fn()
+  until v2 > 0 and (v1*v1 + v2*v2) < 1
+  return dMu + dSigma*v1/v2
+end
+
+random.gamma = function (R, iAlpha)
+  if iAlpha < 1 then error("Alpha < 1") end
+  if iAlpha < 6 then
+    local x = 1.0
+    for i = 1, iAlpha do x = x * R:_fn() end
+    return -math.log(x)  -- TODO check for 0
+  else
+    local x, y, s= 0, 0, 0
+    iAlpha = iAlpha - 1  -- reuse
+    repeat
+      repeat
+        local v1, v2 = 0, 0
+        repeat
+          v1 = R:_fn()
+          v2 = 2*R:_fn()-1.0
+        until v1*v1 + v2*v2 <= 1
+        y = v2 / v1
+        s = math.sqrt(2.0*iAlpha + 1.0)
+        x = s*y + iAlpha
+      until x > 0
+      local e = (1.0 + y*y)*math.exp(iAlpha*math.log(x/iAlpha) - s*y)
+    until R:_fn() < e
+    return x
+  end
+end
+
+random.poisson = function (R, dLam)
+  random.ext_special = random.ext_special or require('lib.special')
+  local gln = random.ext_special.gammaln
+  local em = -1
+  if dLam < 12 then
+    local g = math.exp(-dLam)
+    local t = 1.0
+    repeat
+      t = t * R:_fn()
+      em = em + 1
+    until t <= g
+  else
+    local sq, al, y = math.sqrt(2*dLam), math.log(dLam), 0
+    local g = dLam*al - gln(nil, dLam+1)
+    repeat
+      repeat
+        y = math.tan(math.pi * R:_fn())
+        em = sq*y + dLam
+      until em >= 0
+      em = math.floor(em)
+      local t = 0.9*(1 + y*y)*math.exp(em*al - gln(nil, em + 1)-g)
+    until R:_fn() <= t
+  end
+  return em
+end
+
+random.binomial = function (R, dp, N)
+  random.ext_special = random.ext_special or require('lib.special')
+  local gln = random.ext_special.gammaln
+  local p = (dp < 0.5) and dp or (1 - dp)
+  local bnl, am = 0, p*N
+  if N < 25 then
+    for j = 1, N do
+      if R:_fn() < p then bnl = bnl + 1 end
+    end
+  elseif am < 1.0 then
+    local g, t, j = math.exp(-am), 1.0, 0
+    while j <= N do
+      t = t*R:_fn()
+      if t < g then break end
+      j = j + 1
+    end
+    bnl = (j <= N) and j or N
+  else
+    local pc, og = 1 - p, gln(nil, N + 1)
+    local sq, em = math.sqrt(2*am*pc), 0
+    local plog, pclog = math.log(p), math.log(1 - p)
+    repeat
+      local y = 0
+      repeat
+        y = math.tan(math.pi*R:_fn())
+        em = sq*y + am
+      until 0.0 <= em and em < (N + 1)
+      em = math.floor(em)
+      local t = 1.2*sq*(1 + y*y)*math.exp(
+        og - gln(nil, em + 1) - gln(nil, N - em + 1) + em*plog + (N - em)*pclog)
+    until R:_fn() <= t
+    bnl = em
+  end
+  return (p ~= dp) and (N - bnl) or bnl
+end
 
 random.seed = function (R, N)
   N = N or os.time()  -- set 'arbitrary' value by default
