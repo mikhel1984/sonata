d4de9f3043155814a50c476c3d27b94f100eac7f
Stas <mikhel.sk@gmail.com>
2023-06-16 20:14:19 +0300

    Update comments

--- matlib/qubit.lua
+++ matlib/qubit.lua
@@ -1,26 +1,22 @@
 --[[		sonata/matlib/qubit.lua
 
---- Quantum computing simulation
---  </br></br><b>Authors</b>: Your Name
+--- Quantum computing simulation.
+--
+--  </br></br><b>Authors</b>: Stanislav Mikhel
+--  @release This file is a part of <a href="https://github.com/mikhel1984/sonata">sonata.matlib</a> collection, 2017-2023.
 
 	module 'qubit'
 --]]
 
+---------------- Tests --------------
 --[[TEST
 
 -- use 'qubit'
-Qb = require 'lib.qubit'
+Qb = require 'matlib.qubit'
 
--- example
-a = Qb()
--- check equality
-ans = a.type                  -->  'qubit'
-
--- check relative equality ( ~10^(-2) )
-ans = math.pi                --2> 355/113
+a = 0.2*Qb'|0>' + 0.98*Qb'|1>'
 
 --]]
-
 
 --	LOCAL
 
@@ -31,6 +27,27 @@ Ver = Ver.version
 local Matrix = require('matlib.matrix')
 local Complex = require('matlib.complex')
 
+
+--- Rule for CNOT operation.
+--  @param bits List of booleans.
+--  @param slave Index to change.
+--  @parma master Index to check.
+local function ruleCnot (bits, slave, master)
+  if bits[master] then
+    bits[slave] = not bits[slave]
+  end
+end
+
+
+--- Rule for swap operation.
+--  @param bits List of booleans.
+--  @param i1 Index of the first element.
+--  @param i2 Index of the second element.
+local function ruleSwap (bits, i1, i2)
+  bits[i1], bits[i2] = bits[i2], bits[i1]
+end
+
+
 --	INFO
 
 local help = SonataHelp or {}  -- optional
@@ -40,18 +57,17 @@ __module__ = "Quantum computing simulation"
 }
 
 
---	MODULE
+--	QUBIT
 
 local qubit = {
 -- mark
 type = 'qubit', isqubit = true,
-
+-- bases
 _sign = {
 -- basic
 ['0'] = 1, ['1'] = 1, 
 -- hadamar
-['+'] = 2, ['-'] = 2,
-}
+['+'] = 2, ['-'] = 2}
 }
 -- methametods
 qubit.__index = qubit
@@ -63,38 +79,50 @@ qubit.__index = qubit
 local function isqubit(v) return getmetatable(v) == qubit end
 
 
-qubit._normalize = function (Q)
-  local s = 0
-  local vec = Q.vec
-  for i = 1, vec:rows() do
-    local v = vec[i][1]
-    if getmetatable(v) == Complex then
-      s = s + Cross.float(v * v:conj())
-    else
-      s = s + Cross.float(v)^2
-    end
-  end
-  s = math.sqrt(s)
-  for i = 1, vec:rows() do
-    vec[i][1] = vec[i][1] / s
-  end
-end
-
-qubit._randomize = function (Q)
-  local vec = Q.vec
-  local sum = 0
-  for i = 1, vec:rows() do
-    local v = Complex(2*math.random()-1, 2*math.random()-1)
-    vec[i][1] = v
-    sum = sum + v:re()^2 + v:im()^2
-  end
-  sum = math.sqrt(sum)
-  for i = 1, vec:rows() do
-    vec[i][1] = vec[i][1] / sum
-  end
+--- Q1 + Q2
+--  @param Q1 First state.
+--  @param Q2 Second state.
+--  @return Combined state.
+qubit.__add = function (Q1, Q2)
+  if not (isqubit(Q1) and isqubit(Q2)) then error("Unexpected operation") end
+  if Q1.type ~= Q2.type then error("Different bases") end
+  if Q1.n ~= Q2.n then error("Different size") end
+  return qubit._new(Q1.vec + Q2.vec, Q1.type, Q1.n)
 end
 
 
+--- k * Q
+--  @param C Coefficient.
+--  @param Q State.
+--  @return Scaled state.
+qubit.__mul = function (C, Q)
+  if isqubit(C) and not isqubit(Q) then
+    return qubit.__mul(Q, C)
+  end
+  return qubit._new(C*Q.vec, Q.type, Q.n)
+end
+
+
+--- Q1 .. Q2
+--  @param Q1 First state.
+--  @param Q2 Second state.
+--  @return Combination of states.
+qubit.__concat = function(Q1, Q2) return qubit.combine(Q1, Q2) end
+
+
+--- Initialize new qubit system.
+--  @param v State vector.
+--  @param tp Type of basis.
+--  @param n Number of qubits.
+--  @return new object.
+qubit._new = function (v, tp, n)
+  return setmetatable({vec=v, type=tp, n=n}, qubit)
+end
+
+
+--- Get state from the string representation.
+--  @param state String of the form '|xxx>'.
+--  @return vector, base type, size
 qubit._parse = function (state)
   k = k or 1
   local s = string.match(state, '^|(.+)>$')
@@ -116,24 +144,25 @@ qubit._parse = function (state)
   return res, base, n
 end
 
-qubit._new = function (v, tp, n)
-  return setmetatable({vec=v, type=tp, n=n}, qubit)
-end
 
-qubit.__mul = function (C, Q)
-  if isqubit(C) and not isqubit(Q) then
-    return qubit.__mul(Q, C)
+qubit._randomize = function (Q)
+  local vec = Q.vec
+  local sum = 0
+  for i = 1, vec:rows() do
+    local v = Complex(2*math.random()-1, 2*math.random()-1)
+    vec[i][1] = v
+    sum = sum + v:re()^2 + v:im()^2
   end
-  return qubit._new(C*Q.vec, Q.type, Q.n)
+  sum = math.sqrt(sum)
+  for i = 1, vec:rows() do
+    vec[i][1] = vec[i][1] / sum
+  end
 end
 
-qubit.__add = function (Q1, Q2)
-  if not (isqubit(Q1) and isqubit(Q2)) then error("Unexpected operation") end
-  if Q1.type ~= Q2.type then error("Different bases") end
-  if Q1.n ~= Q2.n then error("Different size") end
-  return qubit._new(Q1.vec + Q2.vec, Q1.type, Q1.n)
-end
 
+--- Combine qubits into the system.
+--  @param ... Sequence of subsystems.
+--  @return combined state.
 qubit.combine = function (...)
   local qs = {...}
   local q1 = qs[1]
@@ -148,10 +177,42 @@ qubit.combine = function (...)
   return qubit._new(v, q1.type, n)
 end
 
-qubit.__concat = function(Q1, Q2) return qubit.combine(Q1, Q2) end
 
+--- Getting copy.
+--  @return Copy of the object.
+qubit.copy = function (Q)
+  -- some logic
+  return qubit._new(Q.vec * 1, Q.type, Q.n)
+end
+about[qubit.copy] = {"Q:copy() --> cpy_Q", "Create a copy of the object."}
+
+
+--- Normalize coefficients to unit vector.
+--  @param Q Qubit system.
+qubit.normalize = function (Q)
+  local s = 0
+  local vec = Q.vec
+  for i = 1, vec:rows() do
+    local v = vec[i][1]
+    if getmetatable(v) == Complex then
+      s = s + Cross.float(v * v:conj())
+    else
+      s = s + Cross.float(v)^2
+    end
+  end
+  s = math.sqrt(s)
+  for i = 1, vec:rows() do
+    vec[i][1] = vec[i][1] / s
+  end
+end
+
+
+--- Get number of qubits.
+--  @param Q Qubit object.
+--  @return number of qubits in the system.
 qubit.size = function (Q) return Q.n end
 qubit.__len = qubit.size
+
 
 -- simplify constructor call
 setmetatable(qubit, {
@@ -160,39 +221,42 @@ __call = function (self, state)
 end
 })
 about[qubit] = {" (t) --> Q", "Create new qubit.", help.NEW}
--- begin from ' ' to get 'Qb ()'
 
 
---- Method example.
---  It is good idea to define method for the copy creation.
---  @return Copy of the object.
-qubit.copy = function (Q)
-  -- some logic
-  return qubit._new(Q.vec * 1, Q.type, Q.n)
-end
-about[qubit.copy] = {"Q:copy() --> cpy_Q",
-  "Create a copy of the object."} -- third element is optional, default is 'base'
+--	QGATE
 
 local qgate = {
-  type = 'qgate', 
+  type = 'qgate',
 }
 qgate.__index = qgate
 
 
+--- Print gates.
+--  @param G System of gates.
+--  @return string representation.
+qgate.__tostring = function (G)
+  local acc = {}
+  for i = G.n, 1, -1 do
+    acc[#acc+1] = string.format('|x%d> %s', i-1, G.txt[i])
+  end
+  return table.concat(acc, '\n')
+end
+
+
+-- Basic matrices.
 qgate._I22 = Matrix:eye(2)
 qgate._X22 = Matrix{{0,1},{1,0}}
 qgate._Y22 = Matrix{{0,Complex:i(-1)}, {Complex:i(1), 0}}
 qgate._Z22 = Matrix{{1, 0}, {0, -1}}
 qgate._S22 = Matrix{{1, 0}, {0, Complex:i(1)}}
 qgate._T22 = Matrix{{1, 0}, {0, Complex:trig(1, math.pi/4)}}
-qgate._H22 = Matrix{{1,1},{1,-1}}  -- multipy 1/sqrt(2)
+qgate._H22 = Matrix{{1,1},{1,-1}}  -- multipy to 1/sqrt(2)
 
-qgate._new = function (n)
-  local txt = {}
-  for i = 1, n do txt[i] = '' end  -- inverted order
-  return setmetatable({n=n, mat=nil, txt=txt}, qgate)
-end
 
+--- Update matrix and representation for basic components.
+--  @param G Gate system.
+--  @param lst List of gates (nil for I).
+--  @param s Symbol to print.
 qgate._fill = function (G, lst, s)
   local txt, res = G.txt, nil
   for i = G.n, 1, -1 do
@@ -208,6 +272,11 @@ qgate._fill = function (G, lst, s)
   G.mat = G.mat and (G.mat * res) or res
 end
 
+
+--- Generate function that works with basic gates.
+--  @param M Gate matrix 2x2.
+--  @param s Symbol to print.
+--  @return function f(G,...)
 qgate._makeGate = function (M, s)
   return function (G, ...)
     local ind, g = {...}, {}
@@ -220,25 +289,16 @@ qgate._makeGate = function (M, s)
   end
 end
 
-qgate._gateX = qgate._makeGate(qgate._X22, 'X')
 
-qgate._gateH = function (G, ...)
-  local g, ind = {}, {...}
-  for _, i in ipairs(ind) do g[i+1] = qgate._H22 end
-  local p = #ind
-  if p == 0 then
-    for i = 1, G.n do g[i] = qgate._H22 end
-    p = G.n
-  end
-  qgate._fill(G, g, 'H')
-  G.mat = G.mat * math.pow(2, -0.5*p)
-  return G
-end
-
-qgate._matrixFor = function (n, rule, ...)
+--- Generate matrix based on rule.
+--  @param G Gate system.
+--  @param rule Funciton that update list of bits (booleans).
+--  @param ... Additional parameters for the rule.
+--  @return matrix for the given rule.
+qgate._matrixFor = function (G, rule, ...)
+  local bits, n = {}, G.n
   local nmax = 2^n
   local mat = Matrix:zeros(nmax, nmax)
-  local bits = {}
   for k = 0, nmax-1 do
     -- to 'bits'
     local v, rst = k, 0
@@ -258,36 +318,25 @@ qgate._matrixFor = function (n, rule, ...)
   return mat
 end
 
-qgate._ruleCnot = function (bits, slave, master)
-  if bits[master] then
-    bits[slave] = not bits[slave]
-  end
+
+--- Prepare empty gate object.
+--  @param n Number of qubits.
+--  @return gate object.
+qgate._new = function (n)
+  local txt = {}
+  for i = 1, n do txt[i] = '' end  -- inverted order
+  return setmetatable({n=n, mat=nil, txt=txt}, qgate)
 end
 
-qgate._gateCnot = function (G, slave_i, master_i)
+
+--- Add gate CNOT.
+--  @param G Gate system.
+--  @param slave_i Index to modify.
+--  @param master_i Index to check.
+--  @return updated object.
+qgate.CNOT = function (G, slave_i, master_i)
   slave_i, master_i = slave_i + 1, master_i + 1
-  local mat = qgate._matrixFor(G.n, qgate._ruleCnot, slave_i, master_i)
-  --local nmax = 2^(G.n)
-  --local mat = Matrix:zeros(nmax, nmax)
-  --local bits = {}
-  --for k = 0, nmax-1 do
-  --  -- to 'bits'
-  --  local v, rst = k, 0
-  --  for i = 1, G.n do
-  --    v, rst = math.modf(v / 2.0)
-  --    bits[i] = (rst > 0.1)  -- use true/false
-  --  end
-  --  -- correct
-  --  if bits[master_i] then
-  --    bits[slave_i] = not bits[slave_i]
-  --  end
-  --  -- to column
-  --  v = 1  -- reuse
-  --  for i, b in ipairs(bits) do
-  --    if b then v = v + 2^(i-1) end
-  --  end
-  --  mat[v][k+1] = 1
-  --end
+  local mat = G:_matrixFor(ruleCnot, slave_i, master_i)
   G.mat = G.mat and (G.mat * mat) or mat
   -- update view
   local txt = G.txt
@@ -298,18 +347,51 @@ qgate._gateCnot = function (G, slave_i, master_i)
     elseif i == master_i then
       s = 'o'
     end
-    txt[i] = string.format('%s %s', txt[i], s)  
+    txt[i] = string.format('%s %s', txt[i], s)
   end
   return G
 end
 
-qgate._ruleSwap = function (bits, i1, i2)
-  bits[i1], bits[i2] = bits[i2], bits[i1]
+
+--- Add gate H.
+--  @param G Gate system.
+--  @param ... Qubit indices to add gate.
+--  @return updated object.
+qgate.H = function (G, ...)
+  local g, ind = {}, {...}
+  for _, i in ipairs(ind) do g[i+1] = qgate._H22 end
+  local p = #ind
+  if p == 0 then
+    for i = 1, G.n do g[i] = qgate._H22 end
+    p = G.n
+  end
+  qgate._fill(G, g, 'H')
+  G.mat = G.mat * math.pow(2, -0.5*p)
+  return G
 end
 
-qgate._gateSwap = function (G, i1, i2)
+
+--- Inverse gate system.
+--  @param G Initial system.
+--  @return inverted version.
+qgate.inverse = function (G)
+  local res = qgate._new(G.n)
+  res.mat = G.mat:H()
+  for i = 1, G.n do
+    res.txt[i] = string.reverse(G.txt[i])
+  end
+  return res
+end
+
+
+--- Swap qubits.
+--  @param G Gate system.
+--  @param i1 First index.
+--  @param i2 Second index.
+--  @return updated object.
+qgate.SWAP = function (G, i1, i2)
   i1, i2 = i1 + 1, i2 + 1
-  local mat = qgate._matrixFor(G.n, qgate._ruleSwap, i1, i2)
+  local mat = G:_matrixFor(ruleSwap, i1, i2)
   G.mat = G.mat and (G.mat * mat) or mat
   local txt = G.txt
   if i1 > i2 then
@@ -327,37 +409,17 @@ qgate._gateSwap = function (G, i1, i2)
   return G
 end
 
-qgate.__tostring = function (G)
-  local acc = {}
-  for i = G.n, 1, -1 do
-    acc[#acc+1] = string.format('|x%d> %s', i-1, G.txt[i])
-  end
-  return table.concat(acc, '\n')
-end
 
-qgate._inverse = function (G)
-  local res = qgate._new(G.n)
-  res.mat = G.mat:H()
-  for i = 1, G.n do
-    res.txt[i] = string.reverse(G.txt[i])
-  end
-  return res
-end
+--- Add gate X.
+--  @param G Gate system.
+--  @param ... Qubit indices to add gate.
+--  @return updated object.
+qgate.X = qgate._makeGate(qgate._X22, 'X')
 
 -- Comment to remove descriptions
 qubit.about = about
 
---return qubit
+return qubit
 
 --======================================
 -- https://en.wikipedia.org/wiki/Quantum_logic_gate 
-
--- inverse gates
-
-gt = qgate._new(3)
-gt:_gateSwap(0,2)
-gt2 = gt:_inverse()
-print(gt.mat)
-print(gt)
-print(gt2.mat)
-print(gt2)
