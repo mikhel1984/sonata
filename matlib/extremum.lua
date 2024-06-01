0f13b088b84c30e66c0179cccdfd950e9ee90307
Stanislav <mikhel.sk@gmail.com>
2024-06-01 16:12:02 +0300

    Add simplex method

--- matlib/extremum.lua
+++ matlib/extremum.lua
@@ -28,6 +28,7 @@ ans = math.pi                --2> 355/113
 
 --	LOCAL
 
+local mabs, mmax, mmin = math.abs, math.max, math.min
 local GOLD = 1.6180339
 local TOL = 1E-4
 
@@ -73,7 +74,7 @@ extremum._bracket = function (fun, a, b)
   while fb > fc do
     local r = (b-a)*(fb-fc)
     local q = (b-c)*(fb-fa)
-    local denom = 2*math.max(math.abs(q-r), tiny)
+    local denom = 2*mmax(mabs(q-r), tiny)
     local u = b - ((b-c)*q - (b-a)*r)/((q >= r) and denom or -denom)
     local ulim = b + limit*(c-b)
     if (b-u)*(u-c) > 0 then   -- parabolic u between b and c
@@ -110,13 +111,13 @@ end
 extremum._minGolden = function (fun, a, b, c)
   local gr, gc = GOLD-1, 2-GOLD
   local x0, x1, x2, x3 = a, nil, nil, c
-  if math.abs(c-b) > math.abs(b-a) then
+  if mabs(c-b) > mabs(b-a) then
     x1, x2 = b, b + gc*(c-b)
   else
     x2, x1 = b, b - gc*(b-a)
   end
   local f1, f2 = fun(x1), fun(x2)
-  while math.abs(x3-x0) > TOL do
+  while mabs(x3-x0) > TOL do
     if f2 < f1 then
       x0, x1, x2 = x1, x2, gr*x2 + gc*x3
       f1, f2 = f2, fun(x2)
@@ -135,29 +136,29 @@ end
 extremum._minBrent = function (fun, a, b, c)
   local imax, small, e, d = 100, 1E-30, 0, 0
   local gc = 2-GOLD
-  a, c = math.min(a, c), math.max(a, c)
+  a, c = mmin(a, c), mmax(a, c)
   local x, w, v = b, b, b
   local fx = fun(x)
   local fv, fw = fx, fx
   for i = 1, imax do
     local xm = 0.5*(a+c)
-    local tol1 = TOL*math.abs(x) + small
+    local tol1 = TOL*mabs(x) + small
     local tol2 = 2*tol1
-    if math.abs(x-xm) <= (tol2-0.5*(c-a)) then
+    if mabs(x-xm) <= (tol2-0.5*(c-a)) then
       return x, fx
     end
     -- parabolic fit
     local upd = false
-    if math.abs(e) > tol1 then
+    if mabs(e) > tol1 then
       local r = (x-w)*(fx-fv)
       local q = (x-v)*(fx-fw)
       local p = (x-v)*q - (x-w)*r
       q = 2*(q-r)
       if q > 0 then p = -p end
-      q = math.abs(q)
+      q = mabs(q)
       local etemp = e
       e = d
-      if math.abs(p) >= math.abs(0.5*q*etemp) or p <= q*(a-x) or p >= q*(c-x) then
+      if mabs(p) >= mabs(0.5*q*etemp) or p <= q*(a-x) or p >= q*(c-x) then
         upd = true
       else
         d = p / q
@@ -173,7 +174,7 @@ extremum._minBrent = function (fun, a, b, c)
       e = (x >= xm) and (a-x) or (c-x)
       d = gc*e
     end
-    local u = (math.abs(d) >= tol1) and (x+d) or (x + (d >= 0 and tol1 or -tol1))
+    local u = (mabs(d) >= tol1) and (x+d) or (x + (d >= 0 and tol1 or -tol1))
     local fu = fun(u)
     if fu <= fx then
       if u >= x then a = x else c = x end
@@ -196,7 +197,7 @@ end
 
 extremum._minBrentD = function (fun, dfun, a, b, c)
   local imax, small, e, d = 100, 1E-30, 0, 0
-  a, c = math.min(a, c), math.max(a, c)
+  a, c = mmin(a, c), mmax(a, c)
   local x, w, v = b, b, b
   local fx = fun(x)
   local fv, fw = fx, fx
@@ -204,14 +205,14 @@ extremum._minBrentD = function (fun, dfun, a, b, c)
   local dv, dw = dx, dx
   for i = 1, imax do
     local xm = 0.5*(a+c)
-    local tol1 = TOL*math.abs(x) + small
+    local tol1 = TOL*mabs(x) + small
     local tol2 = 2*tol1
-    if math.abs(x-xm) <= (tol2-0.5*(c-a)) then
+    if mabs(x-xm) <= (tol2-0.5*(c-a)) then
       return x, fx
     end
     -- parabolic fit
     local upd = false
-    if math.abs(e) > tol1 then
+    if mabs(e) > tol1 then
       local d1 = 2*(c-a)
       local d2 = d1
       if dw ~= dx then d1 = (w-x)*dx/(dx-dw) end
@@ -223,11 +224,11 @@ extremum._minBrentD = function (fun, dfun, a, b, c)
       e = d
       if ok1 or ok2 then
         if ok1 and ok2 then
-          d = (math.abs(d1) < math.abs(d2)) and d1 or d2
+          d = (mabs(d1) < mabs(d2)) and d1 or d2
         else
           d = ok1 and d1 or d2
         end
-        if math.abs(d) <= math.abs(0.5*olde) then
+        if mabs(d) <= mabs(0.5*olde) then
           local u = x + d
           if (u-a < tol2) or (c-u < tol2) then
             d = (xm >= x) and tol1 or (-tol1)
@@ -240,7 +241,7 @@ extremum._minBrentD = function (fun, dfun, a, b, c)
       d = 0.5*e
     end
     local u, fu = nil, nil
-    if math.abs(d) >= tol1 then
+    if mabs(d) >= tol1 then
       u = x + d
       fu = fun(u)
     else
@@ -269,13 +270,110 @@ extremum._minBrentD = function (fun, dfun, a, b, c)
   error('Too many iterations')  --TODO warning
 end
 
+extremum._simplexPsum = function (pp)
+  local psum = {}
+  for i = 1, pp:rows() do
+    local s, pi = 0, pp[i]
+    for j = 1, pp:cols() do s = s + pi[j] end
+    psum[i] = s
+  end
+  return psum
+end
+
+extremum._simplex = function (pp, fun)
+  extremum.ext_matrix = extremum.ext_matrix or require("matlib.matrix")
+  local nmax, small = 200, 1E-10
+  local mat = extremum.ext_matrix
+  local y, pmin = {}, {}
+  for i = 1, pp:cols() do y[i] = fun(pp({}, i)) end
+  local psum = extremum._simplexPsum(pp)
+  local p = pp:copy()
+  local nfunc = 0  -- TODO for loop ?
+  while true do
+    -- find highest, next highest and lowest
+    local ilo = 1
+    local ihi, inhi = 1, 2
+    if y[1] > y[2] then 
+      ihi, inhi = 2, 1
+    end
+    for i = 1, pp:cols() do
+      if y[i] <= y[ilo] then ilo = i end
+      if y[i] > y[ihi] then
+        inhi, ihi = ihi, i
+      elseif y[i] > y[inhi] and i ~= ihi then
+        inhi = i
+      end
+    end
+    local rtol = 2*mabs(y[ihi]-y[ilo])/(mabs(y[ihi]) + mabs(y[ilo]) + small)
+    if rtol < TOL then
+      -- put the best point into first element
+      y[1], y[ilo] = y[ilo], y[1]
+      for i = 1, pp:rows() do
+        local pi = p[i]
+        pi[1], pi[ilo] = pi[ilo], pi[1]
+        pmin[i] = pi[1]
+      end
+      return mat:V(pmin), y[1]
+    end
+    if nfunc >= nmax then error('Too mutch iterations') end
+    -- new iteration
+    local ytry = extremum._simplexExtra(p, y, psum, ihi, -1.0, fun)
+    if ytry <= y[ilo] then
+      -- try additional extrapolation
+      ytry = extremum._simplexExtra(p, y, psum, ihi, 2.0, fun)
+    elseif ytry >= y[inhi] then
+      -- one dimentional construction
+      local ysave = y[ihi]
+      ytry = extremum._simplexExtra(p, y, psum, ihi, 0.5, fun)
+      if ytry >= ysave then
+        for i = 1, p:cols() do
+          if i ~= ilo then
+            local pj = p[j]
+            psum[j] = 0.5*(pj[i] + pj[ilo])
+            pj[i] = psum[j]
+          end
+          y[i] = fun(mat:V(psum))
+        end
+        psum = extremum._simplexPsum(p)
+      end
+    else
+      nfunc = nfunc - 1
+    end
+    nfunc = nfunc + 1
+  end
+end
+
+extremum._simplexExtra = function (p, y, psum, ihi, fac, fun)
+  local ndim = p:rows()
+  local fac1 = (1.0-fac)/ndim
+  local fac2 = fac1 - fac
+  local ptry = {}
+  for j = 1, ndim do ptry[j] = psum[j]*fac1 - p[j][ihi]*fac2 end
+  local ytry = fun(p:V(ptry))  -- use matrix as reference to method V
+  if ytry < y[ihi] then
+    y[ihi] = ytry
+    for j = 1, ndim do
+      psum[j] = psum[j] + ptry[j] - p[j][ihi]
+      p[j][ihi] = ptry[j]
+    end
+  end
+  return ytry
+end
+
 -- Comment to remove descriptions
 extremum.about = about
 
 --return extremum
-local fun = function (x) return x*x end
-local dfun = function (x) return 2*x end
-print(extremum._minBrentD(fun, dfun, -4, -1, 3))
+--local fun = function (x) return x*x end
+--local dfun = function (x) return 2*x end
+--print(extremum._minBrentD(fun, dfun, -4, -1, 3))
 --print(extremum._minBrent(fun, -4, -1, 3))
 --print(extremum._minGolden(fun, -4, -1, 3))
 
+local foo = function (y) return (y[1][1]-1)^2 + (y[2][1]-2)^2 end
+local Mat = require 'matlib.matrix'
+
+pts = Mat{{3,1},{1,-1},{-1,-2}}:T()
+x, fx = extremum._simplex(pts, foo)
+print(x)
+print(fx)
